2d248c72414fdde6a8a125964a57212b5d62cf23
LilTOJustice <muianick4@gmail.com>
2024-09-27 21:48:55 -0700

    Created logger and handled pack loading exceptions

--- src/main/kotlin/liltojustice/trueadaptivemusic/Constants.kt
+++ src/main/kotlin/liltojustice/trueadaptivemusic/Constants.kt
@@ -6,7 +6,7 @@ class Constants {
     companion object {
         const val MUSIC_PACK_DIR = "trueadaptivemusicpacks"
         const val MISC_DIR = ".trueadaptivemusic"
-        const val SELECTED_PACK_DIR = "$MISC_DIR/selectedpack"
+        const val SELECTED_PACK = "$MISC_DIR/selectedpack"
         const val RULES_FILENAME = "rules.json"
         val TRUEADAPTIVEMUSIC_ID = Identifier("trueadaptivemusic:trueadaptivemusicstream")
     }
