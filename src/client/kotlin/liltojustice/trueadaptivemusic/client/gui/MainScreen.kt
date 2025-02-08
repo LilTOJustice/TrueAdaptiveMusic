8d1715b574709e59ca18a8c0780e4a536185e380
LilTOJustice <muianick4@gmail.com>
2025-02-07 23:32:06 -0800

    Remove unused import

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/MainScreen.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/MainScreen.kt
@@ -1,6 +1,5 @@
 package liltojustice.trueadaptivemusic.client.gui
 
-import liltojustice.trueadaptivemusic.Logger
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
 import net.minecraft.client.MinecraftClient
