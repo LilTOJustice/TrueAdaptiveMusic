11912fec8256e7869979f04b039deaf797e5f4eb
LilTOJustice <muianick4@gmail.com>
2025-02-07 22:35:13 -0800

    Null safe GetTrueAdaptiveMusicButton

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/MainScreen.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/MainScreen.kt
@@ -31,10 +31,10 @@ class MainScreen(private val parent: Screen): Screen(Text.literal("True adaptive
     }
 
     companion object {
-        fun GetTrueAdaptiveMusicButton(client: MinecraftClient, parent: Screen): ButtonWidget {
+        fun GetTrueAdaptiveMusicButton(client: MinecraftClient?, parent: Screen): ButtonWidget {
             return ButtonWidget.Builder(Text.literal("True Adaptive Music"))
             {
-                client.setScreen(MainScreen(parent))
+                client?.setScreen(MainScreen(parent))
             }
                 .build()
         }
