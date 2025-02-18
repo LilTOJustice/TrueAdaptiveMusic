d05d2a71dbf448cccd5e2c19a9978eb58f85a748
LilTOJustice <muianick4@gmail.com>
2025-02-17 23:04:33 -0800

    Stop music when new pack selected

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/MusicManager.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/MusicManager.kt
@@ -93,15 +93,9 @@ class MusicManager(
     }
 
     private fun stop() {
-        if (soundInstance != null)
-        {
-            client.soundManager.stop(soundInstance)
-            client.soundManager.stop(oldSoundInstance)
-            fadeInstances.forEach { fadeInstance -> client.soundManager.stop(fadeInstance.soundInstance) }
-            fadeInstances.clear()
-            soundInstance = null
-            oldSoundInstance = null
-        }
+        client.soundManager.stopAll()
+        soundInstance = null
+        oldSoundInstance = null
     }
 
     private fun beginCrossfade(newSoundInstance: SoundInstance) {
