ec6125dd151111b54dc7a2358f9278940f135d47
LilTOJustice <muianick4@gmail.com>
2025-03-05 17:42:32 -0800

    Remove edit UI for Pre-1.20.0

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/screen/MainScreen.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/screen/MainScreen.kt
@@ -1,107 +1,53 @@
 package liltojustice.trueadaptivemusic.client.gui.screen
 
 import liltojustice.trueadaptivemusic.Constants
-import liltojustice.trueadaptivemusic.client.Callbacks
-import liltojustice.trueadaptivemusic.client.ChangeMusicPackCallback
 import liltojustice.trueadaptivemusic.client.gui.widget.PackListWidget
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
-import net.minecraft.client.gui.DrawContext
 import net.minecraft.client.gui.screen.Screen
+import net.minecraft.client.gui.screen.ScreenTexts
 import net.minecraft.client.gui.widget.ButtonWidget
-import net.minecraft.screen.ScreenTexts
+import net.minecraft.client.util.math.MatrixStack
 import net.minecraft.text.Text
-import net.minecraft.util.ActionResult
 import net.minecraft.util.Util
-import java.nio.file.Path
 import kotlin.io.path.Path
-import kotlin.io.path.extension
-import kotlin.io.path.listDirectoryEntries
-import kotlin.io.path.name
 
 @Environment(EnvType.CLIENT)
-class MainScreen(private val parent: Screen): Screen(Text.literal("Music Packs")) {
-    private lateinit var createNewPackButton: ButtonWidget
+class MainScreen(private val parent: Screen): Screen(Text.of("Music Packs")) {
     private lateinit var packListWidget: PackListWidget
     private lateinit var openMusicPacksButton: ButtonWidget
     private lateinit var doneButton: ButtonWidget
-    private lateinit var editButton: ButtonWidget
     private lateinit var refreshButton: ButtonWidget
     private lateinit var wikiButton: ButtonWidget
 
     override fun init() {
-        ChangeMusicPackCallback.EVENT.register { musicPack ->
-            editButton.visible = musicPack != null
-            return@register ActionResult.PASS
-        }
-
-        createNewPackButton = ButtonWidget.Builder(CREATE_PACK_TEXT)
-        {
-            val ongoingEdit = getOngoingEdit()
-            if (ongoingEdit != null) {
-                client?.setScreen(ConfirmBackupScreen(this, ongoingEdit, PackNameScreen(this)))
-                return@Builder
-            }
-
-            client?.setScreen(PackNameScreen(this))
-        }
-            .build()
-        createNewPackButton.width = textRenderer.getWidth(CREATE_PACK_TEXT) + 10
-
-        openMusicPacksButton = ButtonWidget.Builder(OPEN_MUSIC_PACKS_TEXT) {
+        openMusicPacksButton = ButtonWidget(0, 0, 0, 20, OPEN_MUSIC_PACKS_TEXT) {
             Util.getOperatingSystem().open(Path(Constants.MUSIC_PACK_DIR).toUri())
         }
-            .build()
         openMusicPacksButton.width = textRenderer.getWidth(OPEN_MUSIC_PACKS_TEXT) + 10
         openMusicPacksButton.x = width - openMusicPacksButton.width
 
         packListWidget = PackListWidget(
             client!!, this.width, this.height, 48, this.height - 64, 36)
 
-        doneButton = ButtonWidget.builder(ScreenTexts.DONE) { _: ButtonWidget? -> client?.setScreen(parent) }
-            .build()
+        doneButton = ButtonWidget(
+            0, 0, 0, 20, ScreenTexts.DONE) { _: ButtonWidget? -> client?.setScreen(parent) }
         doneButton.width = textRenderer.getWidth(ScreenTexts.DONE) + 10
         doneButton.x = width - doneButton.width
         doneButton.y = height - doneButton.height
 
-        editButton = ButtonWidget.Builder(EDIT_TEXT)
-        {
-            val currentPack = Callbacks.getCurrentMusicPack()
-            val ongoingEdit = getOngoingEdit()
-            val editScreen = EditPackScreen(this, currentPack ?: return@Builder)
-            if (ongoingEdit != null && ongoingEdit.name != currentPack.packName) {
-                client?.setScreen(
-                    ConfirmBackupScreen(
-                        this,
-                        ongoingEdit,
-                        editScreen))
-                return@Builder
-            }
-
-            client?.setScreen(editScreen)
-        }
-            .build()
-        editButton.width = textRenderer.getWidth(EDIT_TEXT) + 10
-        editButton.y = height - editButton.height
-        editButton.visible = Callbacks.getCurrentMusicPack() != null
-
-        refreshButton = ButtonWidget.builder(REFRESH_TEXT) { _: ButtonWidget? -> reload() }
-            .build()
-        refreshButton.y = createNewPackButton.y + createNewPackButton.height + 5
+        refreshButton = ButtonWidget(0, 0, 0, 20, REFRESH_TEXT) { _: ButtonWidget? -> reload() }
         refreshButton.width = textRenderer.getWidth(REFRESH_TEXT) + 10
 
-        wikiButton = ButtonWidget.builder(WIKI_TEXT)
+        wikiButton = ButtonWidget(0, 0, 0, 20, WIKI_TEXT)
         { _: ButtonWidget? -> Util.getOperatingSystem().open(Constants.WIKI_LINK) }
-            .build()
         wikiButton.y = openMusicPacksButton.y + openMusicPacksButton.height + 5
         wikiButton.width = textRenderer.getWidth(WIKI_TEXT) + 10
         wikiButton.x = width - wikiButton.width
 
         addSelectableChild(packListWidget)
-        addDrawableChild(createNewPackButton)
         addDrawableChild(openMusicPacksButton)
         addDrawableChild(doneButton)
-        addDrawableChild(editButton)
         addDrawableChild(refreshButton)
         addDrawableChild(wikiButton)
     }
@@ -110,26 +56,20 @@ class MainScreen(private val parent: Screen): Screen(Text.literal("Music Packs")
         client?.setScreen(parent)
     }
 
-    override fun render(context: DrawContext?, mouseX: Int, mouseY: Int, delta: Float) {
+    override fun render(context: MatrixStack?, mouseX: Int, mouseY: Int, delta: Float) {
         this.packListWidget.render(context, mouseX, mouseY, delta)
-        context?.drawCenteredTextWithShadow(this.textRenderer, this.title, this.width / 2, 8, 16777215)
+        drawCenteredTextWithShadow(
+            context, this.textRenderer, this.title.asOrderedText(), this.width / 2, 8, 0xffffff)
         super.render(context, mouseX, mouseY, delta)
     }
 
-    fun reload() {
+    private fun reload() {
         packListWidget.init()
     }
 
     companion object {
-        fun getOngoingEdit(): Path? {
-            return Path(Constants.MUSIC_PACK_DIR).listDirectoryEntries()
-                .firstOrNull() { file -> file.extension == "new" }
-        }
-
-        private val OPEN_MUSIC_PACKS_TEXT = Text.literal("Open Pack Folder")
-        private val CREATE_PACK_TEXT = Text.literal("Create a new music pack")
-        private val REFRESH_TEXT = Text.literal("Refresh")
-        private val EDIT_TEXT = Text.literal("Edit Pack")
-        private val WIKI_TEXT = Text.literal("Open Wiki")
+        private val OPEN_MUSIC_PACKS_TEXT = Text.of("Open Pack Folder")
+        private val REFRESH_TEXT = Text.of("Refresh")
+        private val WIKI_TEXT = Text.of("Open Wiki")
     }
 }
