f3e6903ce6e8bc92c7a15258a2ee0473dc379132
LilTOJustice <muianick4@gmail.com>
2025-03-08 23:15:02 -0800

    Change param display

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/predicate/PredicateParam.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/predicate/PredicateParam.kt
@@ -2,6 +2,6 @@ package liltojustice.trueadaptivemusic.client.predicate
 
 data class PredicateParam(val name: String, val value: Any) {
     override fun toString(): String {
-        return "$name: $value"
+        return "$name($value)"
     }
 }
