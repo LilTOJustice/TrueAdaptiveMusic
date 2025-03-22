b6586b24e9e626f1653d540b63ed7852f4386f56
LilTOJustice <muianick4@gmail.com>
2025-03-21 22:48:40 -0700

    Restructure sound files for adding new types

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/instance/AdaptiveMusicSoundInstance.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/instance/AdaptiveMusicSoundInstance.kt
@@ -1,6 +1,8 @@
 package liltojustice.trueadaptivemusic.client.instance
 
 import liltojustice.trueadaptivemusic.Constants
+import liltojustice.trueadaptivemusic.client.MusicLoadException
+import liltojustice.trueadaptivemusic.client.sound.SoundFile
 import net.minecraft.client.sound.AbstractSoundInstance
 import net.minecraft.client.sound.AudioStream
 import net.minecraft.client.sound.OggAudioStream
@@ -8,13 +10,15 @@ import net.minecraft.client.sound.SoundInstance
 import net.minecraft.client.sound.SoundLoader
 import net.minecraft.sound.SoundCategory
 import net.minecraft.util.Identifier
-import java.io.InputStream
 import java.util.concurrent.CompletableFuture
 
-class AdaptiveMusicSoundInstance(private val stream: InputStream)
+class AdaptiveMusicSoundInstance(private val soundFile: SoundFile)
     : AbstractSoundInstance(Constants.TRUEADAPTIVEMUSIC_ID, SoundCategory.MUSIC, SoundInstance.createRandom()) {
     override fun getAudioStream (loader: SoundLoader, id: Identifier, repeatInstantly: Boolean):
             CompletableFuture<AudioStream> {
-        return CompletableFuture.completedFuture(OggAudioStream(stream))
+        return when(soundFile.getExtension()) {
+            "ogg" -> CompletableFuture.completedFuture(OggAudioStream(soundFile.getInputStream()))
+            else -> throw MusicLoadException("Invalid extension on file ${soundFile.getName()} (.${soundFile.getExtension()})")
+        }
     }
 }
