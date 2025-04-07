0c814c97b1eba3432873a374db66fe0616796915
LilTOJustice <muianick4@gmail.com>
2025-04-06 21:28:43 -0700

    Switch from temp file to input stream

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/ffmpeg/FFMpegAudioStream.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/ffmpeg/FFMpegAudioStream.kt
@@ -2,38 +2,44 @@ package liltojustice.trueadaptivemusic.client.ffmpeg
 
 import liltojustice.trueadaptivemusic.client.sound.SoundFile
 import net.minecraft.client.sound.AudioStream
-import java.io.File
 import java.nio.ByteBuffer
 import java.nio.ByteOrder
 import javax.sound.sampled.AudioFormat
 
 class FFMpegAudioStream(soundFile: SoundFile, private val format: AudioFormat): AudioStream {
-    private val inputStream = soundFile.getInputStream().use {
-        val tempFile = File.createTempFile("tam", ".tmp")
-        tempFile.writeBytes(it.readAllBytes())
-        tempFile.deleteOnExit()
+    private val ffmpeg = run {
         val ffmpeg = ProcessBuilder(
             "ffmpeg",
             "-v", "panic",
-            "-i", tempFile.absolutePath,
+            "-i", "pipe:0",
             "-f", "s16le",
             "-acodec", "pcm_s16le",
             "-")
             .start()
 
-        ffmpeg.inputStream
+        Thread() {
+            try {
+                soundFile.getInputStream().use {
+                    it.copyTo(ffmpeg.outputStream)
+                }
+                ffmpeg.outputStream.close()
+            }
+            catch (_: Exception) {}
+        }.start()
+
+        ffmpeg
     }
 
     override fun close() {
-        inputStream.close()
+        ffmpeg.destroy()
     }
 
     override fun getFormat(): AudioFormat {
         return format
     }
 
-    override fun getBuffer(size: Int): ByteBuffer {
-        val bytes = inputStream.readNBytes(size)
+    override fun getBuffer(size: Int): ByteBuffer? {
+        val bytes = ffmpeg.inputStream.readNBytes(size)
         val buffer = ByteBuffer.allocateDirect(bytes.size)
         buffer.order(ByteOrder.LITTLE_ENDIAN)
         buffer.put(bytes)
