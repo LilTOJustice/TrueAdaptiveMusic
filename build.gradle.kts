6630ab5293a22a7e8bfda36f95d72f788f748498
LilTOJustice <muianick4@gmail.com>
2025-04-17 00:41:16 -0700

    Fix serialization version

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "2.0.20"
-    kotlin("plugin.serialization") version "2.1.20"
+    kotlin("plugin.serialization") version "2.0.20"
     id("fabric-loom") version "1.7.1"
     id("maven-publish")
 }
