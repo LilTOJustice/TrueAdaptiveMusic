cb09e24a135f31e150d12a12412831d763ccec2a
LilTOJustice <muianick4@gmail.com>
2025-04-19 16:10:52 -0700

    Scroll ClickableTextWidget text on overflow

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/widget/ClickableTextWidget.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/widget/ClickableTextWidget.kt
@@ -14,16 +14,16 @@ open class ClickableTextWidget(
     private val showHighlight: Boolean = true,
     private val onClick: (ClickableTextWidget) -> Unit = {},
     private val isSelected: (ClickableTextWidget) -> Boolean = { false })
-    : ClickableWidget(x, y, 0, 0, Text.literal("Clickable Text Widget")),
+    : ClickableWidget(x, y, 0, 0, Text.literal(text)),
     DataWrapped<ClickableTextWidget> {
     override var customData: Any? = null
     private val textRenderer = MinecraftClient.getInstance().textRenderer
     var color: Int = Colors.WHITE
-    var text = text
-        private set
+    val text: String
+        get() = message.string
 
     init {
-        width = textRenderer.getWidth(text)
+        width = textRenderer.getWidth(message)
         height = textRenderer.fontHeight
     }
 
@@ -46,8 +46,7 @@ open class ClickableTextWidget(
             context?.drawHorizontalLine(x, x + width, y + textRenderer.fontHeight, Colors.WHITE)
         }
 
-        val textRenderer = MinecraftClient.getInstance().textRenderer
-        context?.drawText(textRenderer, text, x, y, color, true)
+        drawScrollableText(context, textRenderer, message, x, y, x + width, y + height, color)
         super.render(context, mouseX, mouseY, delta)
     }
 
@@ -67,8 +66,8 @@ open class ClickableTextWidget(
     }
 
     fun setText(text: String) {
-        this.text = text
-        this.width = textRenderer.getWidth(text)
+        message = Text.literal(text)
+        this.width = textRenderer.getWidth(message)
     }
 
     companion object {
