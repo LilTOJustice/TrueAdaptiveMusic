38e0011ce424b15b44bd5d2576b7187d396ac505
LilTOJustice <muianick4@gmail.com>
2025-08-24 18:35:52 -0700

    Implemented tick delaying with default 2-tick

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/trigger/predicate/MusicPredicate.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/trigger/predicate/MusicPredicate.kt
@@ -5,13 +5,30 @@ import liltojustice.trueadaptivemusic.client.trigger.MusicTrigger
 import net.minecraft.client.MinecraftClient
 
 abstract class MusicPredicate: MusicTrigger() {
-    abstract fun test(client: MinecraftClient): Boolean
+    private var lastResult = false
+
+    protected abstract fun test(client: MinecraftClient): Boolean
+    
+    fun testPredicate(client: MinecraftClient): Boolean {
+        val desiredTickRate = getTickRate()
+        val actualTickRate = if (desiredTickRate < 1) 0 else desiredTickRate
+        val tick = client.server?.ticks ?: 0
+        if (tick % actualTickRate == 0) {
+            lastResult = test(client)
+        }
+
+        return lastResult
+    }
 
     final override fun getTypeName(): String {
         return if (this is ErrorPredicate)
             ErrorPredicate.NAME
         else
             TAMClient.predicateRegistry[this::class]
+    }
+
+    fun getTickRate(): Int {
+        return 2
     }
 
     companion object: MusicPredicateCompanion<MusicPredicate> {
