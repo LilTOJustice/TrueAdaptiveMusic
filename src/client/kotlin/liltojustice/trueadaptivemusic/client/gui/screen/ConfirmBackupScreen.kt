73635faeff16c5e67647cb8dd26ea95acdb232fc
LilTOJustice <muianick4@gmail.com>
2026-01-14 01:18:35 -0800

    Fix blur on backup screen

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/screen/ConfirmBackupScreen.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/screen/ConfirmBackupScreen.kt
@@ -48,6 +48,7 @@ class ConfirmBackupScreen(
 
     override fun render(context: DrawContext?, mouseX: Int, mouseY: Int, delta: Float) {
         renderBackground(context, mouseX, mouseY, delta)
+        super.render(context, mouseX, mouseY, delta)
         context?.drawCenteredTextWithShadow(
             client?.textRenderer,
             Text.translatableWithFallback(
@@ -63,7 +64,6 @@ class ConfirmBackupScreen(
             width / 2,
             height / 2 + textRenderer.fontHeight + 5,
             Colors.WHITE)
-        super.render(context, mouseX, mouseY, delta)
     }
 
     companion object {
