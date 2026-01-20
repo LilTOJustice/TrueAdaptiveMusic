4fdf45a8e5aec2782736b5a2eb5ed8ffea0d4970
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2026-01-19 20:33:31 -0800

    Detect "pausing" even when in a server (#314)

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/music/MusicManager.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/music/MusicManager.kt
@@ -111,14 +111,14 @@ class MusicManager(
             return
         }
 
-        if (client.isPaused && !pauseDone) {
+        if (isPaused(client) && !pauseDone) {
             currentSoundInstance
                 ?.let {
                     volumeManager.startFade(
                         it, PAUSE_FADE_TICKS, PAUSE_VOLUME) }
             pauseDone = true
         }
-        else if (!client.isPaused && pauseDone) {
+        else if (!isPaused(client) && pauseDone) {
             currentSoundInstance
                 ?.let {
                     volumeManager.startFade(
@@ -333,5 +333,9 @@ class MusicManager(
         private const val PAUSE_FADE_TICKS = 5
         private const val BACKGROUND_VOLUME = 0.2F
         private const val PAUSE_VOLUME = 0.2F
+
+        private fun isPaused(client: MinecraftClient): Boolean {
+            return client.world != null && client.currentScreen?.shouldPause() ?: false
+        }
     }
 }
