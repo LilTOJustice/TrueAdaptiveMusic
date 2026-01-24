5ad000afcf6f31539dac722ae3cb39dc2452ec9b
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2026-01-23 23:35:52 -0800

    Fix Export and Improve input widget (#379)
    * Improve text input logic
    
    * Fix export bug

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/widget/utility/TextInputWidget.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/widget/utility/TextInputWidget.kt
@@ -14,7 +14,7 @@ class TextInputWidget(
     private val screen: Screen,
     prompt: String,
     textFieldWidth: Int,
-    onChange: (widget: TextInputWidget, text: String) -> Unit,
+    onChange: (widget: TextInputWidget, text: String) -> String,
     placeholder: String = "",
     x: Int = 0,
     y: Int = 0)
@@ -25,14 +25,20 @@ class TextInputWidget(
     var text: String
         get() { return fieldWidget.text }
         set(value) { fieldWidget.text = value }
+    var updateText: String = ""
 
     init {
-        fieldWidget.setChangedListener { text -> onChange(this, text) }
+        fieldWidget.setChangedListener { text -> updateText = onChange(this, text).ifEmpty { "" } }
         width = promptWidget.width + PADDING + fieldWidget.width
         text = placeholder
     }
 
     override fun renderWidget(context: DrawContext?, mouseX: Int, mouseY: Int, delta: Float) {
+        if (updateText.isNotEmpty()) {
+            text = updateText
+            updateText = ""
+        }
+
         promptWidget.x = x
         promptWidget.y = y
         fieldWidget.x = promptWidget.x + promptWidget.width + PADDING
