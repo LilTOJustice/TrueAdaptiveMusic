999c22734061cec6c0d879d25d7bb9d94810c51a
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2026-02-01 21:06:20 -0800

    Re-enable curseforge (#458)

---
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/trigger/predicate/types/BossHealthPredicate.kt
@@ -0,0 +1,46 @@
+package liltojustice.trueadaptivemusic.client.trigger.predicate.types
+
+import com.google.gson.JsonObject
+import liltojustice.trueadaptivemusic.client.trigger.predicate.MusicPredicate
+import net.minecraft.client.MinecraftClient
+import net.minecraft.util.JsonHelper
+
+class BossHealthPredicate(private val direction: Direction, private val healthPercentage: Int): MusicPredicate() {
+    override fun test(client: MinecraftClient): Boolean {
+        return client.inGameHud.bossBarHud.bossBars.any { bossBar ->
+            healthTest((healthPercentage / 100F), direction, bossBar.value.percent) }
+    }
+
+    override fun getTickRate(): Int {
+        return super.getTickRate() * 4
+    }
+
+    override fun toJson(): JsonObject {
+        val result = JsonObject()
+        result.addProperty("direction", direction.name)
+        result.addProperty("healthPercentage", healthPercentage)
+
+        return result
+    }
+
+    companion object: MusicPredicateCompanion<BossHealthPredicate> {
+        override fun fromJson(json: JsonObject): BossHealthPredicate {
+            return BossHealthPredicate(
+                Direction.valueOf(JsonHelper.getString(json, "direction")),
+                JsonHelper.getInt(json, "healthPercentage"))
+        }
+
+        private fun healthTest(
+            thresholdPercentage: Float, direction: Direction, currentPercentage: Float): Boolean {
+            return when (direction) {
+                Direction.Greater -> currentPercentage > thresholdPercentage
+                Direction.Lesser -> currentPercentage < thresholdPercentage
+            }
+        }
+    }
+
+    enum class Direction {
+        Greater,
+        Lesser
+    }
+}
---
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/trigger/predicate/types/FishingPredicate.kt
@@ -0,0 +1,17 @@
+package liltojustice.trueadaptivemusic.client.trigger.predicate.types
+
+import com.google.gson.JsonObject
+import liltojustice.trueadaptivemusic.client.trigger.predicate.MusicPredicate
+import net.minecraft.client.MinecraftClient
+
+class FishingPredicate: MusicPredicate() {
+    override fun test(client: MinecraftClient): Boolean {
+        return client.player?.fishHook != null
+    }
+
+    companion object: MusicPredicateCompanion<FishingPredicate> {
+        override fun fromJson(json: JsonObject): FishingPredicate {
+            return FishingPredicate()
+        }
+    }
+}
