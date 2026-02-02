d6056432a803a71f863879adf45cd7f4cd1d38cd
LilTOJustice <muianick4@gmail.com>
2026-02-01 20:49:29 -0800

    Re-enable curseforge
    # Conflicts:
    #	src/client/java/liltojustice/trueadaptivemusic/client/mixin/SoundManagerPlayMixin.java
    #	src/client/kotlin/liltojustice/trueadaptivemusic/client/music/MusicManager.kt

---
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/trigger/predicate/types/PausedPredicate.kt
@@ -0,0 +1,17 @@
+package liltojustice.trueadaptivemusic.client.trigger.predicate.types
+
+import com.google.gson.JsonObject
+import liltojustice.trueadaptivemusic.client.trigger.predicate.MusicPredicate
+import net.minecraft.client.MinecraftClient
+
+class PausedPredicate: MusicPredicate() {
+    override fun test(client: MinecraftClient): Boolean {
+        return client.world != null && client.currentScreen?.shouldPause() ?: false
+    }
+
+    companion object: MusicPredicateCompanion<PausedPredicate> {
+        override fun fromJson(json: JsonObject): PausedPredicate {
+            return PausedPredicate()
+        }
+    }
+}
