8beaaf52e23bedd930b695b2634295016a88a288
LilTOJustice <muianick4@gmail.com>
2026-02-01 20:49:29 -0800

    Re-enable curseforge
    # Conflicts:
    #	src/client/java/liltojustice/trueadaptivemusic/client/mixin/SoundManagerPlayMixin.java
    #	src/client/kotlin/liltojustice/trueadaptivemusic/client/music/MusicManager.kt

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/widget/PackStructureWidget.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/widget/PackStructureWidget.kt
@@ -4,7 +4,7 @@ import liltojustice.trueadaptivemusic.Constants
 import liltojustice.trueadaptivemusic.client.gui.extensions.getTriggerTooltipString
 import liltojustice.trueadaptivemusic.client.gui.widget.utility.ClickableTextWidget
 import liltojustice.trueadaptivemusic.client.gui.widget.utility.ContainerWidget
-import liltojustice.trueadaptivemusic.client.music.MusicPack
+import liltojustice.trueadaptivemusic.client.music.pack.MusicPack
 import liltojustice.trueadaptivemusic.client.trigger.event.ErrorEvent
 import liltojustice.trueadaptivemusic.client.trigger.predicate.ErrorPredicate
 import liltojustice.trueadaptivemusic.client.trigger.predicate.MusicPredicateTree
