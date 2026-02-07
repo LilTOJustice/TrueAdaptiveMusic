bfa9b992e0f3898a8ae80b623283a3e0066dc5a5
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2026-02-06 18:52:14 -0800

    1.21.4: Cherry pick commit (#518)
    * TrueAdaptiveMusic 1.5 (#511)
    
    * Fix debug hud and offload predicate compute to TAMClient
    
    * Update tick rate for structure/set
    
    * Fix advancement get event
    
    * Account for guardian combat
    
    * Update clamp ticks
    
    * Music toast integration
    
    * OnPauseEvent
    
    * Update fading function
    
    * Pack meta editing
    
    * Update combat logic
    
    * Combat tweak
    
    * Add music inheritance
    
    * Fix fading bug
    
    * Don't play same song twice if possible
    
    * Fix enter hit crash on PackNameScreen
    
    * Fix pause clamping issue
    
    * Fix on demand playback interference
    
    * Improve ffmpeg performance
    
    * Improve ffmpeg performance and add normalization
    
    * v1.5
    
    * Backport 1.21.4
    
    * Improve robustness of music player
    
    # Conflicts:
    #	src/client/kotlin/liltojustice/trueadaptivemusic/client/music/manager/MusicManager.kt

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/sound/FFmpeg.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/sound/FFmpeg.kt
@@ -10,50 +10,48 @@ import java.io.InputStreamReader
 import javax.sound.sampled.AudioFormat
 import kotlin.io.path.pathString
 
-class FFmpeg {
-    companion object {
-        fun makeStream(soundFile: SoundFile): FFmpegAudioStream {
-            val command = if (TAMClient.hasFFmpegGlobal) "ffprobe" else Constants.FFPROBE_PATH.pathString
-            val ffprobe = ProcessBuilder(
-                command,
-                "-hide_banner",
-                "-i", "pipe:0",
-                "-v", "panic",
-                "-show_streams",
-                "-select_streams", "0",
-                "-print_format", "json")
-                .start()
+object FFmpeg {
+    fun makeStream(soundFile: SoundFile): FFmpegAudioStream {
+        val command = if (TAMClient.hasFFmpegGlobal) "ffprobe" else Constants.FFPROBE_PATH.pathString
+        val ffprobe = ProcessBuilder(
+            command,
+            "-hide_banner",
+            "-i", "pipe:0",
+            "-v", "panic",
+            "-show_streams",
+            "-select_streams", "0",
+            "-print_format", "json")
+            .start()
 
-            // Ignore dumb exception
-            try {
-                soundFile.getInputStream().use {
-                    it.copyTo(ffprobe.outputStream)
-                }
+        // Ignore dumb exception
+        try {
+            soundFile.getInputStream().use {
+                it.copyTo(ffprobe.outputStream)
             }
-            catch (_: Exception) {}
-
-            val reader = BufferedReader(InputStreamReader(ffprobe.inputStream))
-            var line = ""
-            val output = StringBuilder()
-            while (reader.readLine()?.also { line = it } != null) {
-                output.append(line)
-            }
-
-            ffprobe.waitFor()
-
-            val propertyJson = JsonHelper.deserialize(output.toString())
-            val stream = propertyJson["streams"].asJsonArray[0].asJsonObject
-            val channels = stream["channels"].asInt
-            val sampleRate = stream["sample_rate"].asInt
-
-            return FFmpegAudioStream(
-                soundFile,
-                AudioFormat(
-                    sampleRate.toFloat(),
-                    16,
-                    channels,
-                    true,
-                    false))
         }
+        catch (_: Exception) {}
+
+        val reader = BufferedReader(InputStreamReader(ffprobe.inputStream))
+        var line = ""
+        val output = StringBuilder()
+        while (reader.readLine()?.also { line = it } != null) {
+            output.append(line)
+        }
+
+        ffprobe.waitFor()
+
+        val propertyJson = JsonHelper.deserialize(output.toString())
+        val stream = propertyJson["streams"].asJsonArray[0].asJsonObject
+        val channels = stream["channels"].asInt
+        val sampleRate = stream["sample_rate"].asInt
+
+        return FFmpegAudioStream(
+            soundFile,
+            AudioFormat(
+                sampleRate.toFloat(),
+                16,
+                channels,
+                true,
+                false))
     }
 }
