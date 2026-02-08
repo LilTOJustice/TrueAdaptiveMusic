3dcd5867178a7481466c6550495b7df315e22406
LilTOJustice <muianick4@gmail.com>
2026-02-08 02:15:36 -0800

    Fix click drag UI bug

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/widget/PackStructureWidget.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/widget/PackStructureWidget.kt
@@ -90,7 +90,11 @@ class PackStructureWidget(
     }
 
     override fun mouseClicked(mouseX: Double, mouseY: Double, button: Int): Boolean {
-        val result = super.mouseClicked(mouseX, mouseY, button)
+        super.mouseClicked(mouseX, mouseY, button)
+        if (!isMouseOver(mouseX, mouseY)) {
+            return false
+        }
+
         mouseButtonHeld = false
         forEachChild { child ->
             if (child is ClickableTextWidget && child.isMouseOver(mouseX, mouseY)) {
@@ -99,7 +103,7 @@ class PackStructureWidget(
             }
         }
 
-        return result
+        return true
     }
 
     override fun mouseReleased(mouseX: Double, mouseY: Double, button: Int): Boolean {
