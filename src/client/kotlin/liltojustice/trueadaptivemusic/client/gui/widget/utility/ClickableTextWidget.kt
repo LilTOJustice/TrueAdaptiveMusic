87c8991c52ed34a27898af4a40687de287efd3af
LilTOJustice <muianick4@gmail.com>
2026-02-14 00:58:47 -0800

    1.6 Backport to 1.21.7

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/widget/utility/ClickableTextWidget.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/widget/utility/ClickableTextWidget.kt
@@ -64,12 +64,7 @@ open class ClickableTextWidget(
             context?.drawHorizontalLine(x, x + width, y + textRenderer.fontHeight, Colors.WHITE)
         }
 
-        context?.let {
-            drawTextWithMargin(
-                context.getHoverListener(this, DrawContext.HoverType.NONE),
-                coloredText,
-                0)
-        }
+        drawScrollableText(context, textRenderer, coloredText, x, y, x + width, y + height, color)
     }
 
     override fun onClick(mouseX: Double, mouseY: Double) {
