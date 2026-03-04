5930038daa6dcb04a5e290d33189ea096aa2010f
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2026-03-04 00:31:02 -0800

    1.21.8: Cherry pick commit (#630)
    * True Adaptive Music 1.7 (#620)
    
    * Remove minecraft client from test interface
    
    * Migrate to Gson serialization
    
    * Update sourceState access
    
    * Remove Predicate/Event companion generics
    
    * Remove Predicate/Event companion generics
    
    * Fix sound engine errors, remove source cap, and fix sound event warnings
    
    * Rework focusedwidget logic
    
    * Clean up some sound engine code
    
    * Fix a bunch of new UI issues
    
    * Node copying in pack structure widget
    
    * Implement playable sound directory
    
    * Improve UI
    
    * Ambience panning
    
    * Don't play ambience unless in-game and fade it in
    
    * Crossfade ambience when the current track is about to end
    
    * Fix source race conditions
    
    * Reorganize music tree
    
    * Merge to 1.7 WIP: Hybrid Predicates (#617)
    
    * WIP: Hybrid Predicates
    
    * Finish hybrid predicates
    
    * Fix identifier serialization
    
    * Allow loading legacy packs
    
    * Update height predicate
    
    * Dont show + when node isn't selected
    
    * Update pack structure communication
    
    * Improve node editing
    
    * Update changelog
    
    * Rework music event invocation
    
    * Fix debug hud
    
    * Update docs
    
    * Update docs
    
    * Fix Modded Predicates and Events.md
    
    * Re-add java friendly registers
    
    * Re-add java friendly registers
    
    * 1.7 backport to 1.21.8

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/trigger/predicate/types/InBedPredicate.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/trigger/predicate/types/InBedPredicate.kt
@@ -1,17 +1,11 @@
 package liltojustice.trueadaptivemusic.client.trigger.predicate.types
 
-import com.google.gson.JsonObject
 import liltojustice.trueadaptivemusic.client.trigger.predicate.MusicPredicate
 import net.minecraft.client.MinecraftClient
 
 class InBedPredicate: MusicPredicate() {
-    override fun test(client: MinecraftClient): Boolean {
+    override fun test(): Boolean {
+        val client = MinecraftClient.getInstance()
         return client.player?.isSleeping ?: false
-    }
-
-    companion object: MusicPredicateCompanion<InBedPredicate> {
-        override fun fromJson(json: JsonObject): InBedPredicate {
-            return InBedPredicate()
-        }
     }
 }
