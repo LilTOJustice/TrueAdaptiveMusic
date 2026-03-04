369d595d1fcef14ef4f20c01b4d70f831267649a
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2026-03-04 00:31:24 -0800

    1.21.6: Cherry pick commit (#628)
    * True Adaptive Music 1.7 (#620)
    
    * Remove minecraft client from test interface
    
    * Migrate to Gson serialization
    
    * Update sourceState access
    
    * Remove Predicate/Event companion generics
    
    * Remove Predicate/Event companion generics
    
    * Fix sound engine errors, remove source cap, and fix sound event warnings
    
    * Rework focusedwidget logic
    
    * Clean up some sound engine code
    
    * Fix a bunch of new UI issues
    
    * Node copying in pack structure widget
    
    * Implement playable sound directory
    
    * Improve UI
    
    * Ambience panning
    
    * Don't play ambience unless in-game and fade it in
    
    * Crossfade ambience when the current track is about to end
    
    * Fix source race conditions
    
    * Reorganize music tree
    
    * Merge to 1.7 WIP: Hybrid Predicates (#617)
    
    * WIP: Hybrid Predicates
    
    * Finish hybrid predicates
    
    * Fix identifier serialization
    
    * Allow loading legacy packs
    
    * Update height predicate
    
    * Dont show + when node isn't selected
    
    * Update pack structure communication
    
    * Improve node editing
    
    * Update changelog
    
    * Rework music event invocation
    
    * Fix debug hud
    
    * Update docs
    
    * Update docs
    
    * Fix Modded Predicates and Events.md
    
    * Re-add java friendly registers
    
    * Re-add java friendly registers
    
    * 1.7 backport to 1.21.6

--- src/client/java/liltojustice/trueadaptivemusic/client/mixin/event/OnDayStartEventMixin.java
+++ src/client/java/liltojustice/trueadaptivemusic/client/mixin/event/OnDayStartEventMixin.java
@@ -1,7 +1,6 @@
 package liltojustice.trueadaptivemusic.client.mixin.event;
 
 import liltojustice.trueadaptivemusic.client.TAMClient;
-import liltojustice.trueadaptivemusic.client.trigger.event.MusicEvent;
 import liltojustice.trueadaptivemusic.client.trigger.event.types.OnDayStartEvent;
 import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
@@ -15,7 +14,7 @@ public class OnDayStartEventMixin {
     public void tick(CallbackInfo ci) {
         MinecraftClient thisObject = (MinecraftClient)(Object)this;
         if (thisObject.world != null && thisObject.world.getTimeOfDay() % 24000L == 0L) {
-            MusicEvent.Companion.invokeMusicEvent(TAMClient.INSTANCE.getEventRegistry().get(OnDayStartEvent.class));
+            TAMClient.INSTANCE.invokeMusicEvent(OnDayStartEvent.class);
         }
     }
 }
