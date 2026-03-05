2e72c96938d62cebaae554ef95c678104513bd63
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2026-03-05 00:56:10 -0800

    True Adaptive Music 1.7.1 (#660)
    * Only show create node widget if parent node is selected
    
    * Still show create node for root
    
    * Fix event display in Node Widget
    
    * Fix event saving
    
    * Fix event edit issues
    
    * WIP fixing linux zip issue
    
    * Fix deserialization issues
    
    * Version bump

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/serialization/legacy/original/model/trigger/predicate/types/HeightPredicate.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/serialization/legacy/original/model/trigger/predicate/types/HeightPredicate.kt
@@ -2,17 +2,18 @@ package liltojustice.trueadaptivemusic.client.serialization.legacy.original.mode
 
 import com.google.gson.JsonElement
 import com.google.gson.JsonObject
+import com.google.gson.JsonPrimitive
 import liltojustice.trueadaptivemusic.client.serialization.legacy.Convertible
 
 object HeightPredicate: Convertible {
     override fun convert(json: JsonElement): JsonObject {
         val jsonObject = json.asJsonObject
         val result = JsonObject()
-        val above = jsonObject.getAsJsonPrimitive("above").asBoolean
+        val above = jsonObject.getAsJsonPrimitive("above")?.asBoolean ?: false
         val direction = if (above) "Above" else "Below"
 
         result.addProperty("direction", direction)
-        result.add("y", jsonObject.getAsJsonPrimitive("y"))
+        result.add("y", jsonObject.getAsJsonPrimitive("y") ?: JsonPrimitive(0))
 
         return result
     }
