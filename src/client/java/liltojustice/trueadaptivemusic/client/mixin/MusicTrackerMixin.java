893d29501667f335048f4c9b1448ea678d324a7c
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2026-03-21 23:52:05 -0700

    True Adaptive Music 2.0 (#763)
    * Update publish.yml
    
    * Version bump
    
    * Account for teams in scoreboard predicate
    
    * Team predicate
    
    * PlayerAttributePredicate.kt
    
    * PVP predicate
    
    * Prettify comparators
    
    * Add eq/neq for ScoreboardPredicate
    
    * Remove overworld caves from options
    
    * Fix debug hud
    
    * Parallel music
    
    * Fix DropdownWidget
    
    * Improve zip fallback performance
    
    * Fix parallel start volume
    
    * Improve fluid predicates
    
    * Revert clamping logic
    
    * Rudimentary pack browser
    
    * Implement asynchronous loading
    
    * Add async download and improve pack manifest system
    
    * Fix pack downloading and clean up UI
    
    * Fix error type handling in pack editor
    
    * Handle pack download failure
    
    * Download bar
    
    * Fix null in download
    
    * Display descriptions and image in pack browser
    
    * Handle non-png images
    
    * Rename p to process
    
    * Display pack download sizes
    
    * Improve safety of manifest download
    
    * Change source to github gist
    
    * Improve pack presentation
    
    * Improve pack lists
    
    * Add size to pack flavor text
    
    * Update schema and improve download/size styling
    
    * Support updates
    
    * Vanilla behavior node parameter
    
    * Export pack screen
    
    * Consolidate combat and pvp predicates
    
    * Fix zip locking
    
    * Exhaust all music before repeating
    
    * Show playing music/ambience in debug hud
    
    * Improve TextInputWidget.kt
    
    * Allow scoreboard to work with no display on single-player worlds
    
    * Add discord link
    
    * Fix export pack screen and improve ffmpeg stream efficiency
    
    * Improve vanilla behavior node parameter
    
    * Fix image handling
    
    * Improve image handling
    
    * Improve image handling further
    
    * Store reference to sound in sound instance
    
    * Improve music tracking
    
    * Offload predicate testing to separate coroutine
    
    * Update CHANGELOG.md
    
    * Update README.md
    
    * Update CHANGELOG.md
    
    * Async pack loading
    
    * Fix update timezone
    
    * Update wiki
    
    * Update vanillaBehavior -> vanillaMusic
    
    * Avoid conc mod exception with jukebox

--- src/client/java/liltojustice/trueadaptivemusic/client/mixin/MusicTrackerMixin.java
+++ src/client/java/liltojustice/trueadaptivemusic/client/mixin/MusicTrackerMixin.java
@@ -2,7 +2,6 @@ package liltojustice.trueadaptivemusic.client.mixin;
 
 import liltojustice.trueadaptivemusic.client.TAMClient;
 import liltojustice.trueadaptivemusic.client.javasucks.MusicTrackerMixinHelper;
-import liltojustice.trueadaptivemusic.client.sound.instance.AudioFileSoundInstance;
 import net.minecraft.client.sound.*;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -13,24 +12,27 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 @Mixin(MusicTracker.class)
 public class MusicTrackerMixin {
     @Inject(method = "play", at = @At("HEAD"), cancellable = true)
-    public void play(MusicInstance instance, CallbackInfo ci) {
-        var music = instance.music();
-        if (music != null && MusicTrackerMixinHelper.shouldIgnore(music)) {
+    public void play(MusicSound sound, CallbackInfo ci) {
+        TAMClient.INSTANCE.setDesiredVanillaSoundEvent(sound.sound().value());
+        if (MusicTrackerMixinHelper.shouldIgnore(sound)) {
             ci.cancel();
         }
     }
 
     @Inject(method = "getCurrentMusicTranslationKey", at = @At("HEAD"), cancellable = true)
     public void getCurrentMusicTranslationKey(CallbackInfoReturnable<String> cir) {
-        MusicTracker thisObject = (MusicTracker)(Object)this;
-        if (thisObject.current instanceof AudioFileSoundInstance sound) {
-            cir.setReturnValue(sound.getFileName());
+        var currentTAMMusic = TAMClient.INSTANCE.getCurrentMusic();
+        if (currentTAMMusic != null) {
+            cir.setReturnValue(currentTAMMusic.getSoundString());
         }
     }
 
     @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
     public void tick(CallbackInfo ci) {
-        if (TAMClient.INSTANCE.getMusicPack() != null) {
+       var result = TAMClient.INSTANCE.getCurrentPredicateResult();
+        if (TAMClient.INSTANCE.getMusicPack() != null &&
+                result != null &&
+                !result.getParameters().getVanillaMusic()) {
             ci.cancel();
         }
     }
