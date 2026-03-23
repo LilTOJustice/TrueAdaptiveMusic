b29be867f095e8bb03d8101855ae8e3e5c417751
LilTOJustice <muianick4@gmail.com>
2026-03-22 21:34:05 -0700

    2.0 backport to 1.21.5

---
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/music/pack/browsable/MusicPackDownloadException.kt
@@ -0,0 +1,5 @@
+package liltojustice.trueadaptivemusic.client.music.pack.browsable
+
+import liltojustice.trueadaptivemusic.TrueAdaptiveMusicException
+
+class MusicPackDownloadException(message: String?, inner: Exception): TrueAdaptiveMusicException(message, inner)
