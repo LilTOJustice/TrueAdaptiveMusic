d8af65bbc7bbc6aa0f3eabc94ba1bba03cee013b
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2026-03-28 18:09:19 -0700

    True Adaptive Music 2.1 (#790)
    * Pack logos
    
    * Prettify packlist/browser
    
    * Download percentage 1 dpd
    
    * Add vertical marquee for pack text
    
    * Pack export progress bar
    
    * Limit buffer count for non-looping sources
    
    * Package ffmpeg with mod release
    
    * add .gitattributes
    
    * Remove ffmpeg section from wiki
    
    * Audio normalization toggle
    
    * Add author field to browsable packs
    
    * Update .gitignore
    
    * Fix weather legacy deserialization
    
    * Improve performance and fix OS detection
    
    * Update CHANGELOG.md

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/sound/FFmpeg.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/sound/FFmpeg.kt
@@ -1,26 +1,23 @@
 package liltojustice.trueadaptivemusic.client.sound
 
-import liltojustice.trueadaptivemusic.Constants
 import liltojustice.trueadaptivemusic.client.TAMClient
 import net.minecraft.util.JsonHelper
 import java.io.BufferedReader
 import java.io.InputStream
 import java.io.InputStreamReader
 import javax.sound.sampled.AudioFormat
-import kotlin.io.path.pathString
 
 object FFmpeg {
     fun getFileAudioFormat(inputStream: InputStream): AudioFormat {
-        val command = if (TAMClient.hasFFmpegGlobal) "ffprobe" else Constants.FFPROBE_PATH.pathString
         val ffprobe = ProcessBuilder(
-            command,
+            TAMClient.getFFProbeCommand(),
             "-hide_banner",
             "-i", "pipe:0",
             "-v", "panic",
             "-show_streams",
             "-select_streams", "0",
-            "-print_format", "json")
-            .start()
+            "-print_format", "json"
+        ).start()
 
         // Ignore dumb exception
         try {
