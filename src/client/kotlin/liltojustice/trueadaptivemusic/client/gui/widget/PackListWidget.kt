71abf45c8d0fa56b4873caa065f2b4ac1a102236
LilTOJustice <muianick4@gmail.com>
2026-03-28 21:25:46 -0700

    2.1 backport to 1.21.8

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/widget/PackListWidget.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/widget/PackListWidget.kt
@@ -24,6 +24,7 @@ import net.minecraft.text.Text
 import net.minecraft.util.Colors
 import net.minecraft.util.Identifier
 import net.minecraft.util.Util
+import net.minecraft.util.math.MathHelper
 import kotlin.coroutines.EmptyCoroutineContext
 import kotlin.io.path.extension
 import kotlin.io.path.nameWithoutExtension
@@ -53,6 +54,30 @@ class PackListWidget(
 
     override fun getRowRight(): Int {
         return right - 16
+    }
+
+    override fun drawSelectionHighlight(
+        context: DrawContext,
+        y: Int,
+        entryWidth: Int,
+        entryHeight: Int,
+        borderColor: Int,
+        fillColor: Int
+    ) {
+        val i = rowLeft
+        val j = rowRight
+        context.fill(i, y - 2, j, y + entryHeight + 2, borderColor)
+        context.fill(i + 1, y - 1, j - 1, y + entryHeight + 1, fillColor)
+    }
+
+    override fun getEntryAtPosition(x: Double, y: Double): Entry? {
+        val j = rowLeft
+        val k = rowRight
+        val m = MathHelper.floor(y - this.y.toDouble()) - this.headerHeight + this.scrollY.toInt() - 4
+        val n = m / this.itemHeight
+        return this.children().takeIf {
+            x >= j.toDouble() && x <= k.toDouble() && m >= 0 && n < this.entryCount
+        }?.get(n)
     }
 
     override fun renderWidget(context: DrawContext?, mouseX: Int, mouseY: Int, deltaTicks: Float) {
@@ -160,8 +185,6 @@ class PackListWidget(
     }
 
     open inner class Entry(private val musicPack: MusicPack?): AlwaysSelectedEntryListWidget.Entry<Entry>() {
-        private val imageSize
-            get() = height
         private val issuesButton =
             if (musicPack?.validationMessages?.isEmpty() != false) {
                 null
@@ -173,12 +196,12 @@ class PackListWidget(
                 result
             }
 
-        override fun getWidth(): Int {
+        fun getWidth(): Int {
             return scrollbarX - rowLeft - 3
         }
 
         override fun render(
-            context: DrawContext?,
+            context: DrawContext,
             index: Int,
             y: Int,
             x: Int,
@@ -190,14 +213,16 @@ class PackListWidget(
             tickDelta: Float
         ) {
             musicPack ?: return
-            renderPackImage(context)
+            val entryWidth = getWidth()
             val packName = musicPack.packPath.nameWithoutExtension
             val extension = musicPack.packPath.extension
-            context.textConsumer.marqueedText(
+            renderPackImage(context,  x, y, entryHeight)
+            drawScrollableText(
+                context,
+                client.textRenderer,
                 prettyPackNameText(extension != "zip", packName),
-                x + 5 + imageSize,
-                x + 5 + imageSize,
-                rowRight - 3,
+                x + 5 + entryHeight,
+                x + 5 + entryHeight,
                 y + 3,
                 rowRight - 3,
                 y + client.textRenderer.fontHeight + 3,
@@ -209,11 +234,12 @@ class PackListWidget(
                 issuesButton.render(context, mouseX, mouseY, tickDelta)
             }
 
-            context.textConsumer.marqueedText(
+            drawScrollableText(
+                context,
+                client.textRenderer,
                 Text.literal(musicPack.options.description).withColor(Colors.GRAY),
-                x + 5 + imageSize,
-                x + 5 + imageSize,
-                (issuesButton?.x ?: rowRight) - 3,
+                x + 5 + entryHeight,
+                x + 5 + entryHeight,
                 y + 17,
                 (issuesButton?.x ?: rowRight) - 3,
                 y + entryHeight,
@@ -241,7 +267,7 @@ class PackListWidget(
             return Text.empty()
         }
 
-        private fun renderPackImage(context: DrawContext) {
+        private fun renderPackImage(context: DrawContext, entryX: Int, entryY: Int, imageSize: Int) {
             musicPack ?: return
             val identifier = Identifier.of(
                 "trueadaptivemusic",
@@ -264,8 +290,8 @@ class PackListWidget(
             context.drawTexture(
                 RenderPipelines.GUI_TEXTURED,
                 identifier,
-                x + 2,
-                y,
+                entryX + 2,
+                entryY,
                 0F,
                 0F,
                 imageSize,
@@ -277,7 +303,18 @@ class PackListWidget(
     }
 
     inner class VanillaEntry: Entry(null) {
-        override fun render(context: DrawContext, mouseX: Int, mouseY: Int, hovered: Boolean, tickDelta: Float) {
+        override fun render(
+            context: DrawContext,
+            index: Int,
+            y: Int,
+            x: Int,
+            entryWidth: Int,
+            entryHeight: Int,
+            mouseX: Int,
+            mouseY: Int,
+            hovered: Boolean,
+            tickDelta: Float
+        ) {
             context.drawText(
                 client.textRenderer,
                 VANILLA_TEXT,
@@ -286,7 +323,7 @@ class PackListWidget(
                 Colors.WHITE,
                 false
             )
-            context?.drawText(
+            context.drawText(
                 client.textRenderer,
                 DISABLE_TAM_TEXT,
                 x + 3, y + 14 + 3,
@@ -304,7 +341,7 @@ class PackListWidget(
         }
 
         override fun render(
-            context: DrawContext?,
+            context: DrawContext,
             index: Int,
             y: Int,
             x: Int,
@@ -315,7 +352,7 @@ class PackListWidget(
             hovered: Boolean,
             tickDelta: Float
         ) {
-            context?.drawText(
+            context.drawText(
                 client.textRenderer,
                 PACK_BROWSER_TEXT,
                 x + 3,
