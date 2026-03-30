951f22eb7a096a16f5a964501e373328087f7f7a
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2026-03-30 10:02:11 -0700

    2.1 Hotfix 3 (#853) (#862)
    * Fix posix file permissions and file creation
    
    * Improve stream handling
    
    * Fix process termination for ffmpeg
    
    * Account for thread interrupt in channel
    
    * Sleep rather than park channel thread
    
    * Another attempt to fix thread blocking
    
    * Go back

--- src/main/kotlin/liltojustice/trueadaptivemusic/TrueAdaptiveMusic.kt
+++ src/main/kotlin/liltojustice/trueadaptivemusic/TrueAdaptiveMusic.kt
@@ -1,47 +1,51 @@
 package liltojustice.trueadaptivemusic
 
 import net.fabricmc.api.ModInitializer
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.attribute.FileAttribute
+import java.nio.file.attribute.PosixFilePermission
+import java.nio.file.attribute.PosixFilePermissions
 import kotlin.io.path.*
 
 class TrueAdaptiveMusic: ModInitializer {
     @OptIn(ExperimentalPathApi::class)
     override fun onInitialize() {
         Files.createDirectories(Constants.MUSIC_PACK_DIR)
-        Files.createDirectories(Constants.OPTIONS_DIR)
+        Files.createDirectories(Constants.FFMPEG_DIR)
         Files.createDirectories(Constants.PACK_BROWSER_CACHE_DIR)
 
-        Constants.FFMPEG_WINDOWS_PATH.toFile().takeIf { !it.exists() }?.let { file ->
-            file.createNewFile()
-            this::class.java.classLoader.getResourceAsStream(Constants.FFMPEG_WINDOWS_RESOURCE_PATH).use {
-                it?.copyTo(file.outputStream())
-            }
+        if (isWindows) {
+            cloneResourceFile(Constants.FFMPEG_WINDOWS_PATH, Constants.FFMPEG_WINDOWS_RESOURCE)
+            cloneResourceFile(Constants.FFPROBE_WINDOWS_PATH, Constants.FFPROBE_WINDOWS_RESOURCE)
         }
-
-        Constants.FFPROBE_WINDOWS_PATH.toFile().takeIf { !it.exists() }?.let { file ->
-            file.createNewFile()
-            this::class.java.classLoader.getResourceAsStream(Constants.FFPROBE_WINDOWS_RESOURCE_PATH).use {
-                it?.copyTo(file.outputStream())
-            }
-        }
-
-        Constants.FFMPEG_PATH.toFile().takeIf { !it.exists() }?.let { file ->
-            file.createNewFile()
-            this::class.java.classLoader.getResourceAsStream(Constants.FFMPEG_RESOURCE_PATH).use {
-                it?.copyTo(file.outputStream())
-            }
-        }
-
-        Constants.FFPROBE_PATH.toFile().takeIf { !it.exists() }?.let { file ->
-            file.createNewFile()
-            this::class.java.classLoader.getResourceAsStream(Constants.FFPROBE_RESOURCE_PATH).use {
-                it?.copyTo(file.outputStream())
-            }
+        else {
+            cloneResourceFile(Constants.FFMPEG_PATH, Constants.FFMPEG_RESOURCE)
+            cloneResourceFile(Constants.FFPROBE_PATH, Constants.FFPROBE_RESOURCE)
         }
     }
 
     companion object {
-        val LOGGER: org.slf4j.Logger = LoggerFactory.getLogger(TrueAdaptiveMusic::class.java)
+        val LOGGER: Logger = LoggerFactory.getLogger(TrueAdaptiveMusic::class.java)
+        val POSIX_PERMISSIONS: FileAttribute<Set<PosixFilePermission>> = PosixFilePermissions.asFileAttribute(
+            PosixFilePermissions.fromString("rwxrwxrwx"))
+        val isWindows = "windows" in System.getProperty("os.name").lowercase()
+
+        fun cloneResourceFile(destinationPath: Path, resource: String) {
+            destinationPath.takeIf { !it.exists() }?.let { filePath ->
+                if (isWindows) {
+                    Files.createFile(filePath)
+                }
+                else {
+                    Files.createFile(filePath, POSIX_PERMISSIONS)
+                }
+
+                this::class.java.classLoader.getResourceAsStream(resource).use {
+                    it?.copyTo(filePath.outputStream())
+                }
+            }
+        }
     }
 }
