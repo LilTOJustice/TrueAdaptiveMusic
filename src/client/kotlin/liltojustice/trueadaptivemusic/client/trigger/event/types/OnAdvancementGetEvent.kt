9ead0bc9b13ec43ea540d0af897713294576015a
LilTOJustice <muianick4@gmail.com>
2026-03-31 23:27:47 -0700

    Clean up trigger code

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/trigger/event/types/OnAdvancementGetEvent.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/trigger/event/types/OnAdvancementGetEvent.kt
@@ -2,5 +2,5 @@ package liltojustice.trueadaptivemusic.client.trigger.event.types
 
 import liltojustice.trueadaptivemusic.client.trigger.event.MusicEvent
 
-class OnAdvancementGetEvent(): MusicEvent() {
+class OnAdvancementGetEvent: MusicEvent() {
 }
--- src/client/kotlin/liltojustice/trueadaptivemusic/client/trigger/predicate/types/WeatherPredicate.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/trigger/predicate/types/WeatherPredicate.kt
@@ -22,8 +22,7 @@ class WeatherPredicate(private val weather: Weather): MusicPredicate() {
     companion object: MusicPredicateCompanion {
         override val argDescriptions: Map<String, String>
             get() = super.argDescriptions + mapOf(
-                "weather" to "Which weather the music should play for."
-            )
+                WeatherPredicate::weather.name to "Which weather the music should play for.")
     }
 
     enum class Weather {
