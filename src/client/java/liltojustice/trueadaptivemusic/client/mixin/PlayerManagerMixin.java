c5826ce274845a9d78c2a1bce89a897617bff31c
LilTOJustice <muianick4@gmail.com>
2026-04-03 03:31:40 -0700

    Now launchable, needs many tests/fixes

--- src/client/java/liltojustice/trueadaptivemusic/client/mixin/PlayerManagerMixin.java
+++ src/client/java/liltojustice/trueadaptivemusic/client/mixin/PlayerManagerMixin.java
@@ -3,14 +3,15 @@ package liltojustice.trueadaptivemusic.client.mixin;
 import net.minecraft.network.protocol.Packet;
 import net.minecraft.server.ServerScoreboard;
 import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.players.PlayerList;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(Server.class)
+@Mixin(PlayerList.class)
 public class PlayerManagerMixin {
-    @Inject(method = "sendScoreboard", at = @At("TAIL"))
+    @Inject(method = "updateEntireScoreboard", at = @At("TAIL"))
     public void sendScoreboard(ServerScoreboard scoreboard, ServerPlayer player, CallbackInfo ci) {
         scoreboard
                 .getObjectives()
