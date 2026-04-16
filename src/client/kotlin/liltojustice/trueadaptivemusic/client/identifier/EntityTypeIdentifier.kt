fb96a3bac1ec668fe9c3df642492e230310fee7e
LilTOJustice <muianick4@gmail.com>
2026-04-16 16:52:32 -0700

    2.1.2 backport to 1.21.1

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/identifier/EntityTypeIdentifier.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/identifier/EntityTypeIdentifier.kt
@@ -1,9 +1,8 @@
 package liltojustice.trueadaptivemusic.client.identifier
 
-import net.minecraft.core.registries.BuiltInRegistries
-import net.minecraft.resources.Identifier
-import net.minecraft.world.entity.Entity
-import kotlin.jvm.optionals.getOrNull
+import net.minecraft.entity.Entity
+import net.minecraft.registry.Registries
+import net.minecraft.util.Identifier
 
 class EntityTypeIdentifier(id: Identifier): TypedIdentifier(id) {
     override fun toPrefixedTranslationKey(): String {
@@ -11,17 +10,17 @@ class EntityTypeIdentifier(id: Identifier): TypedIdentifier(id) {
     }
 
     fun matches(entity: Entity): Boolean {
-        return BuiltInRegistries.ENTITY_TYPE[id].getOrNull()?.let {
-            entity.`is`(it)
-        } ?: BuiltInRegistries.ENTITY_TYPE.tags.toList().firstOrNull { it.key().location == id }?.key()?.let {
-            entity.`is`(it)
-        } ?: false
+        val entityType = entity.type
+        return entityType == id ||
+                Registries.ENTITY_TYPE.streamTags().toList().firstOrNull { it.id == id }?.let {
+                    entityType.isIn(it)
+                } ?: false
     }
 
     companion object: TypedIdentifierCompanion() {
         override fun getRegistryIds(): List<Identifier> {
-            return BuiltInRegistries.ENTITY_TYPE.keySet().toList() +
-                    BuiltInRegistries.ENTITY_TYPE.tags.map { it.key().location }.toList()
+            return Registries.ENTITY_TYPE.keys.map { it.value }.toList() +
+                    Registries.ENTITY_TYPE.streamTags().toList().map { it.id }.toList()
         }
     }
 }
