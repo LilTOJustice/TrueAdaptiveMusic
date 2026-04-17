e96b7fdc6257590253ffd0219b7228df895ab59d
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2026-04-16 17:13:19 -0700

    1.21.5: Cherry pick commit (#906)
    * True Adaptive Music 2.1.2 (#898)
    
    * Fix serialization issues
    
    * Don't propagate non-fatal concurrency errors
    
    * Add exception logging for curl
    
    * Decrease lfs usage
    
    * Entity tag support
    
    * Fix download zipfile disposal
    
    * fix format
    
    * Fix sound reset handling in 26.1
    
    * Fix format
    
    * Fix music not persisting when new node contains current music
    
    * Stop considering enterDelay when persisting music
    
    * Fix time handling in 26.1
    
    * Fix path handling for linux
    
    * Fix pack browser image scaling
    
    * Version bump 2.1.2
    
    * Rename workflow
    
    * Push back bugfix
    
    * 2.1.2 backport to 1.21.5

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/identifier/EntityTypeIdentifier.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/identifier/EntityTypeIdentifier.kt
@@ -1,5 +1,6 @@
 package liltojustice.trueadaptivemusic.client.identifier
 
+import net.minecraft.entity.Entity
 import net.minecraft.registry.Registries
 import net.minecraft.util.Identifier
 
@@ -8,9 +9,18 @@ class EntityTypeIdentifier(id: Identifier): TypedIdentifier(id) {
         return id.toTranslationKey("entity")
     }
 
+    fun matches(entity: Entity): Boolean {
+        val entityType = entity.type
+        return entityType == id ||
+                Registries.ENTITY_TYPE.tags.toList().firstOrNull { it.tag.id == id }?.tag?.let {
+                    entityType.isIn(it)
+                } ?: false
+    }
+
     companion object: TypedIdentifierCompanion() {
         override fun getRegistryIds(): List<Identifier> {
-            return Registries.ENTITY_TYPE.ids.toList()
+            return Registries.ENTITY_TYPE.keys.map { it.value }.toList() +
+                    Registries.ENTITY_TYPE.tags.map { it.tag.id }.toList()
         }
     }
 }
