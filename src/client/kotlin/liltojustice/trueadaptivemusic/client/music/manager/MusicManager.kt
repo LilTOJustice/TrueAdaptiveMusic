e050aab0bb66fe10bc65262220dff65b4e2dc527
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2026-04-16 17:12:45 -0700

    1.21.9: Cherry pick commit (#910)
    * True Adaptive Music 2.1.2 (#898)
    
    * Fix serialization issues
    
    * Don't propagate non-fatal concurrency errors
    
    * Add exception logging for curl
    
    * Decrease lfs usage
    
    * Entity tag support
    
    * Fix download zipfile disposal
    
    * fix format
    
    * Fix sound reset handling in 26.1
    
    * Fix format
    
    * Fix music not persisting when new node contains current music
    
    * Stop considering enterDelay when persisting music
    
    * Fix time handling in 26.1
    
    * Fix path handling for linux
    
    * Fix pack browser image scaling
    
    * Version bump 2.1.2
    
    * Rename workflow
    
    * Push back bugfix
    
    * 2.1.2 backport to 1.21.9

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/music/manager/MusicManager.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/music/manager/MusicManager.kt
@@ -206,7 +206,7 @@ class MusicManager(private val client: MinecraftClient) {
 
         updatePredicateId(identifier)
 
-        if (shouldKeepPlaying(musicToPlay, enterDelay, persistNodeMusic)) {
+        if (shouldKeepPlaying(musicToPlay, persistNodeMusic)) {
             return
         }
 
@@ -261,11 +261,9 @@ class MusicManager(private val client: MinecraftClient) {
                 && musicVolumeOption.value > 0
     }
 
-    private fun shouldKeepPlaying(
-        musicToPlay: List<PlayableSound>, enterDelay: UInt, persistNodeMusic: Boolean): Boolean {
+    private fun shouldKeepPlaying(musicToPlay: List<PlayableSound>, persistNodeMusic: Boolean): Boolean {
         val mainTrackPlaying = musicPlayer.isTrackPlaying(mainTrack)
-        return mainTrackPlaying &&
-                ((musicToPlay.contains(currentMusic?.playableSound) && enterDelay != 0U) || persistNodeMusic)
+        return mainTrackPlaying && (musicToPlay.contains(currentMusic?.playableSound) || persistNodeMusic)
     }
 
     private fun getRandomDelay(trackDelay: UInt, trackDelayNoise: UInt): UInt {
@@ -400,16 +398,20 @@ class MusicManager(private val client: MinecraftClient) {
                     musicPlayer.isTrackPlaying(ON_DEMAND_TRACK)
         }
 
-        private fun jukeboxPlaying(client: MinecraftClient): Boolean {
-            val instances = client.soundManager.soundSystem.sources.keys.toMutableSet()
-            return instances.any { instance ->
-                ((instance.category == SoundCategory.RECORDS)
-                        && (instance is PositionedSoundInstance)
-                        && (client.player?.let {
-                    Vec3d(instance.x, instance.y, instance.z)
-                        .squaredDistanceTo(it.entityPos) <
-                            (instance.sound?.attenuation ?: 0) * (instance.sound?.attenuation ?: 0) * 4
-                } ?: false))
+        private fun jukeboxPlaying(minecraft: MinecraftClient): Boolean {
+            return try {
+                minecraft.soundManager.soundSystem.sources.keys.any { instance ->
+                    ((instance.category == SoundCategory.RECORDS)
+                            && (instance is PositionedSoundInstance)
+                            && (minecraft.player?.let {
+                        Vec3d(instance.x, instance.y, instance.z)
+                            .squaredDistanceTo(it.entityPos) <
+                                (instance.sound?.attenuation ?: 0) * (instance.sound?.attenuation ?: 0) * 4
+                    } ?: false))
+                }
+            }
+            catch (_: ConcurrentModificationException) {
+                false
             }
         }
 
