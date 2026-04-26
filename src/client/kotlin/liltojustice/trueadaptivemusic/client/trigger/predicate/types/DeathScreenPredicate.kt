61aebbc1488ad3965f875c40f48bb1ceef7a6690
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2026-04-26 16:53:26 -0700

    2.2 backport to 1.21.9 (#935)
    # Conflicts:
    #	gradle.properties

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/trigger/predicate/types/DeathScreenPredicate.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/trigger/predicate/types/DeathScreenPredicate.kt
@@ -1,10 +1,10 @@
 package liltojustice.trueadaptivemusic.client.trigger.predicate.types
 
-import liltojustice.trueadaptivemusic.client.trigger.predicate.MusicPredicate
+import liltojustice.trueadaptivemusicapi.trigger.predicate.type.BasicPredicateType
 import net.minecraft.client.MinecraftClient
 import net.minecraft.client.gui.screen.DeathScreen
 
-class DeathScreenPredicate: MusicPredicate() {
+object DeathScreenPredicate: BasicPredicateType("death_screen") {
     override fun test(): Boolean {
         val client = MinecraftClient.getInstance()
         return client.currentScreen is DeathScreen
