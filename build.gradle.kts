3159cc193778a3a745b226f9add310d9494bb62f
LilTOJustice <muianick4@gmail.com>
2026-04-29 23:45:19 -0700

    2.2-hotfix2 backport to 1.20.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -60,8 +60,8 @@ dependencies {
     // Fabric API. This is technically optional, but you probably want it anyway.
     modImplementation("net.fabricmc.fabric-api:fabric-api:${project.property("fabric_api_version")}")
 
-    api("com.terraformersmc:modmenu:${project.property("modMenu_version")}")
-    api("curse.maven:trueadaptivemusicapi-1514598:8013281")
+    modApi("com.terraformersmc:modmenu:${project.property("modMenu_version")}")
+    modApi("curse.maven:trueadaptivemusicapi-1514598:8013287")
 }
 
 tasks.processResources {
