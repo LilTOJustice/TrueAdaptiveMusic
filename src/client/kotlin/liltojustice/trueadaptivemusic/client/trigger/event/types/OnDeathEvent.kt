73496d439181dc2af6d7bf535bb9ee0cbcb26b4d
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2026-04-29 22:50:04 -0700

    True Adaptive Music 2.2-hotfix2 (#973)
    * Fix on_death
    
    * Fix identifier deserialization
    
    * Improve deserialization fix

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/trigger/event/types/OnDeathEvent.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/trigger/event/types/OnDeathEvent.kt
@@ -2,4 +2,4 @@ package liltojustice.trueadaptivemusic.client.trigger.event.types
 
 import liltojustice.trueadaptivemusicapi.trigger.event.type.BasicEventType
 
-object OnDeathEvent: BasicEventType("on_death_event")
+object OnDeathEvent: BasicEventType("on_death")
