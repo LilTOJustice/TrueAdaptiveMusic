69378a508bd20055f3acaf1840cd8539ec6d7a3b
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2026-04-29 23:46:21 -0700

    1.21.10: Cherry pick commit (#989)
    * True Adaptive Music 2.2-hotfix2 (#973)
    
    * Fix on_death
    
    * Fix identifier deserialization
    
    * Improve deserialization fix
    
    * Reverting null safety to save a headache
    
    * 2.2-hotfix2 backport to 1.21.11
    
    * Fix sound engine mixin

--- build.gradle.kts
+++ build.gradle.kts
@@ -61,7 +61,7 @@ dependencies {
     modImplementation("net.fabricmc.fabric-api:fabric-api:${project.property("fabric_api_version")}")
 
     modApi("com.terraformersmc:modmenu:${project.property("modMenu_version")}")
-    modApi("curse.maven:true-adaptive-music-api-1514598:7985977")
+    modApi("curse.maven:trueadaptivemusicapi-1514598:8013286")
 }
 
 tasks.processResources {
