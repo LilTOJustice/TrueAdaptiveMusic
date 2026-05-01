c2b289c1b64e9bcf8764cf6c9b1a17af82c6f8d5
LilTOJustice <muianick4@gmail.com>
2026-04-30 23:27:29 -0700

    True Adaptive Music 2.2.1 (#1004)
    * Update workflows
    
    * Improve publish
    
    * Add maven central publishing
    
    * Temporarily remove mod publishing for test
    
    * Remove maven central publish
    
    * Re-add music pack browser
    
    * Update constants
    
    * Update readme
    # Conflicts:
    #	src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/screen/MissingPackBrowserScreen.kt
    
    # Conflicts:
    #	src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/extensions/DrawContextExtensions.kt

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/extensions/DrawContextExtensions.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/extensions/DrawContextExtensions.kt
@@ -1,4 +1,4 @@
-package liltojustice.trueadaptivemusic.client.gui.text
+package liltojustice.trueadaptivemusic.client.gui.extensions
 
 import net.minecraft.client.font.TextRenderer
 import net.minecraft.client.gui.DrawContext
