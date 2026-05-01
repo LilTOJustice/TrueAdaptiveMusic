ec23d47b8f9edbb765b4912973050dc48dffa264
LilTOJustice <muianick4@gmail.com>
2026-04-30 20:26:53 -0700

    Add maven central publishing

--- build.gradle.kts
+++ build.gradle.kts
@@ -107,11 +107,14 @@ publishing {
         }
     }
 
-    // See https://docs.gradle.org/current/userguide/publishing_maven.html for information on how to set up publishing.
     repositories {
-        // Add repositories to publish to here.
-        // Notice: This block does NOT have the same function as the block in the top level.
-        // The repositories here will be used for publishing your artifact, not for
-        // retrieving dependencies.
+        maven {
+            name = "MavenCentral"
+            url = URI("https://repo1.maven.org/maven2")
+            credentials {
+                username = System.getenv("CENTRAL_USERNAME")
+                password = System.getenv("CENTRAL_PASSWORD")
+            }
+        }
     }
 }
