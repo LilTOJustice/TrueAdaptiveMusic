8fe81c52049e97151935bddb58598dbc9323d34f
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2026-04-30 23:53:34 -0700

    True Adaptive Music 2.2.1 (#1004) (#1010)
    * Update workflows
    
    * Improve publish
    
    * Add maven central publishing
    
    * Temporarily remove mod publishing for test
    
    * Remove maven central publish
    
    * Re-add music pack browser
    
    * Update constants
    
    * Update readme
    # Conflicts:
    #	src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/screen/MissingPackBrowserScreen.kt
    
    # Conflicts:
    #	src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/extensions/DrawContextExtensions.kt

---
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/browser/MusicPackDownloadException.kt
@@ -0,0 +1,5 @@
+package liltojustice.trueadaptivemusic.client.browser
+
+import liltojustice.trueadaptivemusic.TrueAdaptiveMusicException
+
+class MusicPackDownloadException(message: String?, inner: Exception): TrueAdaptiveMusicException(message, inner)
