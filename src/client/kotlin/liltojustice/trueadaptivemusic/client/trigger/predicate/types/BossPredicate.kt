632b46475d357f2de60151346599fd6fa86ac57f
LilTOJustice <muianick4@gmail.com>
2026-05-01 21:51:48 -0700

    2.2.1-hotfix1 fix broken boss predicate for 1.21.11 down

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/trigger/predicate/types/BossPredicate.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/trigger/predicate/types/BossPredicate.kt
@@ -17,8 +17,8 @@ object BossPredicate: StaticPredicateType<BossPredicate.Arguments>(
         )
 
     override fun test(arguments: Arguments): Boolean {
-        return Minecraft.getInstance().gui.bossOverlay.events.values.any { bossBar ->
-            val bossName = (bossBar.name.contents as? TranslatableContents)?.key ?: return@any false
+        return MinecraftClient.getInstance().inGameHud.bossBarHud.bossBars.values.any { bossBar ->
+            val bossName = (bossBar.name.content as? TranslatableTextContent)?.key ?: return@any false
             arguments.bosses.isEmpty() ||
                     arguments.bosses.any { boss -> bossName == boss.toLanguageKey("entity") }
         }
