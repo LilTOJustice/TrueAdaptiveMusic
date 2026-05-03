6077b2752709b1d834065cfecbed40891f265285
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2026-05-02 17:02:55 -0700

    True Adaptive Music 2.3 (#1037)
    * Version bump 2.3
    
    * Update modrinth pack browser url
    
    (cherry picked from commit 2d13e18810b20b0b77b01f1a74428653218d2c77)
    
    * Improve game mode predicate
    
    * Remove TAMClient API wrapping
    
    * Code cleanup pass
    
    * True Adaptive Music 2.2-hotfix2 (#973)
    
    * Fix on_death
    
    * Fix identifier deserialization
    
    * Improve deserialization fix
    
    (cherry picked from commit 43b2302131159feff673615012ad0804e5938b3a)
    
    * True Adaptive Music 2.2.1 (#1004) (#1005)
    
    * Update workflows
    
    * Improve publish
    
    * Add maven central publishing
    
    * Temporarily remove mod publishing for test
    
    * Remove maven central publish
    
    * Re-add music pack browser
    
    * Update constants
    
    * Update readme
    
    * Remove pack browser recommendation
    
    * Add enum type adapter
    
    * Add image processor
    
    * Update refresh time
    
    * Bump api version
    
    * Server-side structure predicate
    
    * Spawn nearby predicate
    
    * Cache server-side processor results
    
    * Cache properly by not sending packet on cache time
    
    * Optimize predicate caching logic
    
    * Fix scoreboard player name
    
    * Fix warden combat
    
    * Custom predicate
    
    * Biome tags
    
    * Update changelog

--- src/client/java/liltojustice/trueadaptivemusic/client/mixin/event/OnNightStartMixin.java
+++ src/client/java/liltojustice/trueadaptivemusic/client/mixin/event/OnNightStartMixin.java
@@ -1,8 +1,8 @@
 package liltojustice.trueadaptivemusic.client.mixin.event;
 
-import liltojustice.trueadaptivemusic.client.TAMClient;
 import liltojustice.trueadaptivemusic.client.trigger.event.types.OnNightStartEvent;
-import net.minecraft.client.MinecraftClient;
+import liltojustice.trueadaptivemusicapi.TAMAPI;
+import net.minecraft.client.Minecraft;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,9 +12,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class OnNightStartMixin {
     @Inject(at = @At("HEAD"), method = "tick()V")
     public void tick(CallbackInfo ci) {
-        MinecraftClient thisObject = (MinecraftClient)(Object)this;
-        if (thisObject.world != null && thisObject.world.getTimeOfDay() % 24000L == 13000L) {
-            TAMClient.INSTANCE.invokeMusicEvent(OnNightStartEvent.INSTANCE);
+        Minecraft thisObject = (Minecraft)(Object)this;
+        if (thisObject.level != null && thisObject.level.getGameTime() % 24000L == 13000L) {
+            TAMAPI.INSTANCE.invokeEvent(OnNightStartEvent.INSTANCE);
         }
     }
 }
