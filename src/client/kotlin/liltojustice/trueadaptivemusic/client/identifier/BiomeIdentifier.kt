0b845bd5cd5103aeced7eb9b51cca9b846507365
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2026-05-03 13:09:01 -0700

    1.21.1: Cherry pick commit (#1039)
    * True Adaptive Music 2.3 (#1037)
    
    * Version bump 2.3
    
    * Update modrinth pack browser url
    
    (cherry picked from commit 2d13e18810b20b0b77b01f1a74428653218d2c77)
    
    * Improve game mode predicate
    
    * Remove TAMClient API wrapping
    
    * Code cleanup pass
    
    * True Adaptive Music 2.2-hotfix2 (#973)
    
    * Fix on_death
    
    * Fix identifier deserialization
    
    * Improve deserialization fix
    
    (cherry picked from commit 43b2302131159feff673615012ad0804e5938b3a)
    
    * True Adaptive Music 2.2.1 (#1004) (#1005)
    
    * Update workflows
    
    * Improve publish
    
    * Add maven central publishing
    
    * Temporarily remove mod publishing for test
    
    * Remove maven central publish
    
    * Re-add music pack browser
    
    * Update constants
    
    * Update readme
    
    * Remove pack browser recommendation
    
    * Add enum type adapter
    
    * Add image processor
    
    * Update refresh time
    
    * Bump api version
    
    * Server-side structure predicate
    
    * Spawn nearby predicate
    
    * Cache server-side processor results
    
    * Cache properly by not sending packet on cache time
    
    * Optimize predicate caching logic
    
    * Fix scoreboard player name
    
    * Fix warden combat
    
    * Custom predicate
    
    * Biome tags
    
    * Update changelog
    
    * Fix predicate copying
    
    * 2.3 backport to 1.21.1

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/identifier/BiomeIdentifier.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/identifier/BiomeIdentifier.kt
@@ -1,18 +1,38 @@
 package liltojustice.trueadaptivemusic.client.identifier
 
 import net.minecraft.client.MinecraftClient
+import net.minecraft.registry.Registry
 import net.minecraft.registry.RegistryKeys
+import net.minecraft.registry.entry.RegistryEntry
 import net.minecraft.util.Identifier
+import net.minecraft.world.biome.Biome
+import kotlin.jvm.optionals.getOrNull
 
 class BiomeIdentifier(id: Identifier): TypedIdentifier(id) {
     override fun toPrefixedLanguageKey(): String {
         return id.toTranslationKey("biome")
     }
 
+    fun matches(biome: RegistryEntry<Biome>): Boolean {
+        val registry = getBiomeRegistry() ?: return false
+        return registry.streamTags().toList().firstOrNull { it.id == id }?.let {
+            biome.isIn(it)
+        } ?: (registry[id] == biome.value())
+    }
+
     companion object: TypedIdentifierCompanion() {
         override fun getRegistryIds(): List<Identifier> {
-            return MinecraftClient
-                .getInstance().world?.registryManager?.get(RegistryKeys.BIOME)?.ids?.toList() ?: listOf()
+            val registry = getBiomeRegistry() ?: return emptyList()
+
+            return registry.keys.toList().map { it.value } +
+                    registry.streamTags().toList().map { it.id }.filter { it?.namespace != "c" }.toList()
+        }
+
+        private fun getBiomeRegistry(): Registry<Biome>? {
+            return MinecraftClient.getInstance().world
+                ?.registryManager
+                ?.getOptional(RegistryKeys.BIOME)
+                ?.getOrNull()
         }
     }
 }
