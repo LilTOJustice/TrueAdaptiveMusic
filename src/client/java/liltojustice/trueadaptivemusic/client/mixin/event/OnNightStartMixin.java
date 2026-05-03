e340b85574662e77242f83063bcc7f0352ea3bc9
LilTOJustice <muianick4@gmail.com>
2026-05-03 11:06:28 -0700

    2.3 backport to 1.21.2

--- src/client/java/liltojustice/trueadaptivemusic/client/mixin/event/OnNightStartMixin.java
+++ src/client/java/liltojustice/trueadaptivemusic/client/mixin/event/OnNightStartMixin.java
@@ -2,7 +2,7 @@ package liltojustice.trueadaptivemusic.client.mixin.event;
 
 import liltojustice.trueadaptivemusic.client.trigger.event.types.OnNightStartEvent;
 import liltojustice.trueadaptivemusicapi.TAMAPI;
-import net.minecraft.client.Minecraft;
+import net.minecraft.client.MinecraftClient;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -12,8 +12,8 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public class OnNightStartMixin {
     @Inject(at = @At("HEAD"), method = "tick()V")
     public void tick(CallbackInfo ci) {
-        Minecraft thisObject = (Minecraft)(Object)this;
-        if (thisObject.level != null && thisObject.level.getGameTime() % 24000L == 13000L) {
+        MinecraftClient thisObject = (MinecraftClient) (Object)this;
+        if (thisObject.world != null && thisObject.world.getTimeOfDay() % 24000L == 13000L) {
             TAMAPI.INSTANCE.invokeEvent(OnNightStartEvent.INSTANCE);
         }
     }
