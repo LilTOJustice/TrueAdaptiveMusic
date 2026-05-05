ae3a1462e73c04b10878b780ddecdba52059067f
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2026-05-04 20:09:02 -0700

    True Adaptive Music 2.3.1 (#1079)
    * Improve compatibility
    
    * Version bump
    
    * Version bump
    
    * Update changelog
    
    * Update changelog

--- src/client/java/liltojustice/trueadaptivemusic/client/mixin/SoundManagerMixin.java
+++ src/client/java/liltojustice/trueadaptivemusic/client/mixin/SoundManagerMixin.java
@@ -2,8 +2,11 @@ package liltojustice.trueadaptivemusic.client.mixin;
 
 import liltojustice.trueadaptivemusic.client.TAMClient;
 import liltojustice.trueadaptivemusic.client.javasucks.SoundManagerMixinHelper;
-import net.minecraft.client.sound.*;
-import net.minecraft.sound.SoundCategory;
+import net.minecraft.client.resources.sounds.SoundInstance;
+import net.minecraft.client.sounds.SoundEngine;
+import net.minecraft.client.sounds.SoundManager;
+import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundSource;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -11,10 +14,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(SoundManager.class)
 public class SoundManagerMixin {
-    @Inject(method = "play(Lnet/minecraft/client/sound/SoundInstance;)V", at = @At("HEAD"), cancellable = true)
-    public void play(SoundInstance sound, CallbackInfo ci) {
-        if (SoundManagerMixinHelper.shouldIgnore(sound)) {
-            ci.cancel();
+    @Inject(method = "play", at = @At("HEAD"), cancellable = true)
+    public void play(SoundInstance instance, CallbackInfoReturnable<SoundEngine.PlayResult> cir) {
+        SoundManager thisObject = (SoundManager)(Object)this;
+
+        if (instance.getSource() == SoundSource.MUSIC) {
+            TAMClient.INSTANCE.setDesiredVanillaSoundEvent(
+                    SoundEvent.createVariableRangeEvent(instance.getIdentifier()));
+        }
+
+        if (SoundManagerMixinHelper.shouldIgnore(instance)) {
+            instance.resolve(thisObject);
+            cir.setReturnValue(SoundEngine.PlayResult.STARTED);
         }
     }
 
