52e91d8f4347f9639118a837145653fc426dfdfc
LilTOJustice <muianick4@gmail.com>
2026-05-04 22:09:09 -0700

    True Adaptive Music 2.3.1 (#1079)
    * Improve compatibility
    
    * Version bump
    
    * Version bump
    
    * Update changelog
    
    * Update changelog

--- src/client/java/liltojustice/trueadaptivemusic/client/mixin/SoundManagerMixin.java
+++ src/client/java/liltojustice/trueadaptivemusic/client/mixin/SoundManagerMixin.java
@@ -2,11 +2,10 @@ package liltojustice.trueadaptivemusic.client.mixin;
 
 import liltojustice.trueadaptivemusic.client.TAMClient;
 import liltojustice.trueadaptivemusic.client.javasucks.SoundManagerMixinHelper;
-import net.minecraft.client.resources.sounds.SoundInstance;
-import net.minecraft.client.sounds.SoundEngine;
-import net.minecraft.client.sounds.SoundManager;
-import net.minecraft.sounds.SoundEvent;
-import net.minecraft.sounds.SoundSource;
+import net.minecraft.client.sound.SoundInstance;
+import net.minecraft.client.sound.SoundManager;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvent;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,18 +13,17 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(SoundManager.class)
 public class SoundManagerMixin {
-    @Inject(method = "play", at = @At("HEAD"), cancellable = true)
-    public void play(SoundInstance instance, CallbackInfoReturnable<SoundEngine.PlayResult> cir) {
+    @Inject(method = "play(Lnet/minecraft/client/sound/SoundInstance;)V", at = @At("HEAD"), cancellable = true)
+    public void play(SoundInstance sound, CallbackInfo ci) {
         SoundManager thisObject = (SoundManager)(Object)this;
 
-        if (instance.getSource() == SoundSource.MUSIC) {
-            TAMClient.INSTANCE.setDesiredVanillaSoundEvent(
-                    SoundEvent.createVariableRangeEvent(instance.getIdentifier()));
+        if (sound.getCategory() == SoundCategory.MUSIC) {
+            TAMClient.INSTANCE.setDesiredVanillaSoundEvent(SoundEvent.of(sound.getId()));
         }
 
-        if (SoundManagerMixinHelper.shouldIgnore(instance)) {
-            instance.resolve(thisObject);
-            cir.setReturnValue(SoundEngine.PlayResult.STARTED);
+        if (SoundManagerMixinHelper.shouldIgnore(sound)) {
+            sound.getSoundSet(thisObject);
+            ci.cancel();
         }
     }
 
