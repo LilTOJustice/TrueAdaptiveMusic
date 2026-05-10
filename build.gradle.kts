ea2be4eb87e9c6c333fbfe7804720952c6f8edde
LilTOJustice <muianick4@gmail.com>
2026-05-10 03:59:49 -0700

    2.4.1 - Update API versioning compatibility

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,7 +57,7 @@ dependencies {
     implementation("net.fabricmc.fabric-api:fabric-api:${project.property("fabric_api_version")}")
 
     api("com.terraformersmc:modmenu:${project.property("modMenu_version")}")
-    api("curse.maven:trueadaptivemusicapi-1514598:8051436")
+    api("curse.maven:trueadaptivemusicapi-1514598:8067846")
 }
 
 tasks.processResources {
