d12b6d754382bee416b3d31efb2761e62208a617
LilTOJustice <muianick4@gmail.com>
2026-05-10 05:45:57 -0700

    Update api

--- build.gradle.kts
+++ build.gradle.kts
@@ -57,8 +57,8 @@ dependencies {
     // Fabric API. This is technically optional, but you probably want it anyway.
     modImplementation("net.fabricmc.fabric-api:fabric-api:${project.property("fabric_api_version")}")
 
-    api("com.terraformersmc:modmenu:${project.property("modMenu_version")}")
-    api("curse.maven:trueadaptivemusicapi-1514598:8067846")
+    modApi("com.terraformersmc:modmenu:${project.property("modMenu_version")}")
+    modApi("curse.maven:trueadaptivemusicapi-1514598:8067838")
 }
 
 tasks.processResources {
