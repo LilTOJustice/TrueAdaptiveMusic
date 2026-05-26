021f7e0221580dea0444432c205a6934e1ce9c7b
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2026-05-25 18:04:55 -0700

    True Adaptive Music 2.5.0 - Backport to 1.21.11 (#1178)

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/widget/EventViewWidget.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/widget/EventViewWidget.kt
@@ -18,6 +18,7 @@ import net.minecraft.client.gui.tooltip.Tooltip
 import net.minecraft.registry.Registries
 import net.minecraft.text.Text
 import net.minecraft.util.Colors
+import net.minecraft.util.Identifier
 import java.util.Timer
 import kotlin.concurrent.schedule
 import kotlin.reflect.full.primaryConstructor
@@ -157,12 +158,14 @@ class EventViewWidget(
                     Text.translatableWithFallback(
                         "trueadaptivemusic.select_track", "Select tracks").string,
                     selectedMusicPaths,
-                    onHoverOption = { option ->
+                    { option ->
                         TAMClient.playSoundNow(option?.let { PlayableSound.of(it, soundLibrary) })
                     },
                     tooltipText = Text.translatableWithFallback(
                         "trueadaptivemusic.music_choice.description",
-                        "Select any amount of music to be chosen randomly to play")
+                        "Select any amount of music to be chosen randomly to play"
+                    ),
+                    customCreator = { text -> Identifier.tryParse(text)?.toString() }
                 )
             },
             "musicChoice"
