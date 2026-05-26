5884a5f476bbffd6860bf6f59dccd882c49bab0a
LilTOJustice <muianick4@gmail.com>
2026-05-25 17:10:54 -0700

    Fix spacing

--- src/client/java/liltojustice/trueadaptivemusic/client/mixin/MusicTrackerMixin.java
+++ src/client/java/liltojustice/trueadaptivemusic/client/mixin/MusicTrackerMixin.java
@@ -11,7 +11,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MusicTracker.class)
 public class MusicTrackerMixin {
-    @Inject(method = "play", at = @At("HEAD")   , cancellable = true)
+    @Inject(method = "play", at = @At("HEAD"), cancellable = true)
     public void play(MusicSound type, CallbackInfo ci) {
         TAMClient.INSTANCE.setDesiredVanillaSoundEvent(type.getSound().value());
         if (MusicTrackerMixinHelper.shouldIgnore(type)) {
