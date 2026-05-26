5057db43a7546524af5161b547d222d461322659
LilTOJustice <muianick4@gmail.com>
2026-05-25 17:13:46 -0700

    True Adaptive Music 2.5.0 - Backport to 1.21.2

--- src/client/java/liltojustice/trueadaptivemusic/client/mixin/MusicTrackerMixin.java
+++ src/client/java/liltojustice/trueadaptivemusic/client/mixin/MusicTrackerMixin.java
@@ -11,20 +11,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(MusicTracker.class)
 public class MusicTrackerMixin {
-    @Inject(method = "play", at = @At("HEAD")   , cancellable = true)
+    @Inject(method = "play", at = @At("HEAD"), cancellable = true)
     public void play(MusicSound type, CallbackInfo ci) {
         TAMClient.INSTANCE.setDesiredVanillaSoundEvent(type.getSound().value());
         if (MusicTrackerMixinHelper.shouldIgnore(type)) {
-            ci.cancel();
-        }
-    }
-
-    @Inject(method = "tick", at = @At("HEAD"), cancellable = true)
-    public void tick(CallbackInfo ci) {
-        var result = TAMClient.INSTANCE.getCurrentPredicateResult();
-        if (TAMClient.INSTANCE.getMusicPack() != null &&
-                result != null &&
-                !result.getParameters().getVanillaMusic()) {
             ci.cancel();
         }
     }
