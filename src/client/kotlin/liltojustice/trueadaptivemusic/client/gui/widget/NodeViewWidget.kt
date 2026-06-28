8807c331c1be29a9539b5b6a9d0c28ee9df9e0f6
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2026-06-28 10:44:24 -0700

    1.21.11: Cherry pick commit (#1204)
    * True Adaptive Music 2.6.0 (#1200)
    
    * Version bump 2.6.0
    
    * OnFluidPredicate
    
    * Communicate and filter allowed file types
    
    * Create disable fade option
    
    * Fix linux filestream leak preventing music from playing
    
    * Disable fading node parameter
    
    * Update CHANGELOG.md and api source
    
    * Update CHANGELOG.md and api source
    
    * 2.6.0 backport to 1.21.11

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/widget/NodeViewWidget.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/widget/NodeViewWidget.kt
@@ -19,6 +19,7 @@ import net.minecraft.client.gui.DrawContext
 import net.minecraft.client.gui.screen.narration.NarrationMessageBuilder
 import net.minecraft.client.gui.tooltip.Tooltip
 import net.minecraft.registry.Registries
+import net.minecraft.text.MutableText
 import net.minecraft.text.Text
 import net.minecraft.util.Colors
 import net.minecraft.util.Identifier
@@ -185,9 +186,7 @@ class NodeViewWidget(
                                 onChange()
                             },
                             width,
-                            Text.translatableWithFallback(
-                                "trueadaptivemusic.music_choice", "Music Choice"
-                            ).string,
+                            Constants.MUSIC_CHOICE_TEXT.string,
                             getOptions = {
                                 musicPack.getEditPackSoundLibrary().map { (assetName, _) -> assetName }.toMutableSet()
                                     .union(
@@ -198,16 +197,11 @@ class NodeViewWidget(
                                     .sorted()
                             },
                             notSelectedPlaceholder = selectedMusicPaths.firstOrNull()
-                                ?: Text
-                                    .translatableWithFallback(
-                                        "trueadaptivemusic.select_track", "Select tracks").string,
+                                ?: Constants.SELECT_TRACKS_TEXT.string,
                             onHoverOption = { option ->
                                 TAMClient.playSoundNow(option?.let { PlayableSound.of(it, soundLibrary) })
                             },
-                            tooltipText = Text.translatableWithFallback(
-                                "trueadaptivemusic.music_choice.description",
-                                "Select any amount of music to be chosen randomly to play"
-                            )
+                            tooltipText = Constants.MUSIC_CHOICE_TOOLTIP_TEXT
                         )
                     }
                     else {
@@ -220,9 +214,7 @@ class NodeViewWidget(
                                 clearLoopIntroEndpointWidgets()
                                 onChange()
                             },
-                            Text.translatableWithFallback(
-                                "trueadaptivemusic.music_choice", "Music Choice"
-                            ).string,
+                            Constants.MUSIC_CHOICE_TEXT.string,
                             {
                                 musicPack.getEditPackSoundLibrary().map { (assetName, _) -> assetName }.toMutableSet()
                                     .union(
@@ -232,17 +224,12 @@ class NodeViewWidget(
                                     )
                                     .sorted()
                             },
-                            Text.translatableWithFallback(
-                                "trueadaptivemusic.select_track", "Select tracks"
-                            ).string,
+                            Constants.SELECT_TRACKS_TEXT.string,
                             selectedMusicPaths,
                             { option ->
                                 TAMClient.playSoundNow(option?.let { PlayableSound.of(it, soundLibrary) })
                             },
-                            Text.translatableWithFallback(
-                                "trueadaptivemusic.music_choice.description",
-                                "Select any amount of music to be chosen randomly to play"
-                            ),
+                            Constants.MUSIC_CHOICE_TOOLTIP_TEXT,
                             customCreator = { text -> Identifier.tryParse(text)?.toString() }
                         )
                     }
@@ -261,25 +248,21 @@ class NodeViewWidget(
                         selectedAmbiencePaths = selected.toMutableList()
                         onChange()
                     },
-                    Text.translatableWithFallback(
-                        "trueadaptivemusic.ambience_choice", "Ambience Choice").string,
+                    AMBIENCE_CHOICE_TEXT.string,
                     {
                         musicPack.getEditPackSoundLibrary().map { (assetName, _) -> assetName }.toMutableSet()
                             .union(
                                 Registries.SOUND_EVENT.ids
                                     .map { id -> id.toString() }
-                                    .filter { path -> path.contains("music.") }
+                                    .filter { path -> path.contains("ambient.") }
                             )
                             .toList()
                     },
-                    Text.translatableWithFallback(
-                        "trueadaptivemusic.select_track", "Select tracks").string,
+                    Constants.SELECT_TRACKS_TEXT.string,
                     selectedAmbiencePaths,
                     { option ->
                         TAMClient.playSoundNow(option?.let { PlayableSound.of(it, soundLibrary) }) },
-                    Text.translatableWithFallback(
-                        "trueadaptivemusic.ambience_choice.description",
-                        "Select any amount of ambience to be chosen randomly to play"),
+                    AMBIENCE_CHOICE_TOOLTIP_TEXT,
                     customCreator = { text -> Identifier.tryParse(text)?.toString() }
                 )
             },
@@ -601,6 +584,13 @@ class NodeViewWidget(
     }
 
     companion object {
+        val AMBIENCE_CHOICE_TEXT: MutableText = Text.translatableWithFallback(
+            "trueadaptivemusic.ambience_choice", "Ambience Choice")
+        val AMBIENCE_CHOICE_TOOLTIP_TEXT: MutableText = Text.translatableWithFallback(
+            "trueadaptivemusic.ambience_choice.description",
+            "Select any amount of ambience to be chosen randomly to play"
+        ).append("\n\n").append(Constants.ALLOWED_FILE_TYPES_TEXT)
+
         private fun enforceParameterConstraints(musicPack: MusicPack, node: MusicTree.Node): Boolean {
             node.parent?.let {
                 if (it.parameters.parallelMusic) {
