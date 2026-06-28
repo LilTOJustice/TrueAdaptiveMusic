96b553cc0ff51a866f46ff8be11d1eea15c497cb
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2026-06-28 10:44:41 -0700

    1.21.9: Cherry pick commit (#1212)
    * True Adaptive Music 2.6.0 (#1200)
    
    * Version bump 2.6.0
    
    * OnFluidPredicate
    
    * Communicate and filter allowed file types
    
    * Create disable fade option
    
    * Fix linux filestream leak preventing music from playing
    
    * Disable fading node parameter
    
    * Update CHANGELOG.md and api source
    
    * 2.6.0 backport to 1.21.9

--- build.gradle.kts
+++ build.gradle.kts
@@ -58,7 +58,7 @@ dependencies {
     modImplementation("net.fabricmc.fabric-api:fabric-api:${project.property("fabric_api_version")}")
 
     modApi("com.terraformersmc:modmenu:${project.property("modMenu_version")}")
-    modApi("curse.maven:trueadaptivemusicapi-1514598:8142850")
+    modApi("curse.maven:trueadaptivemusicapi-1514598:8333448")
 }
 
 tasks.processResources {
