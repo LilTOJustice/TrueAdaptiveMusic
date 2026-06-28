d1fa6d071507884a3fc9510876e7ee1d80e5317c
LilTOJustice <muianick4@gmail.com>
2026-06-28 10:24:35 -0700

    2.6.0 backport to 1.21.6

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/widget/Constants.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/widget/Constants.kt
@@ -1,20 +1,20 @@
 package liltojustice.trueadaptivemusic.client.gui.widget
 
 import liltojustice.trueadaptivemusic.Constants.Companion.ALLOWED_FILE_TYPES
-import net.minecraft.network.chat.Component
-import net.minecraft.network.chat.MutableComponent
+import net.minecraft.text.MutableText
+import net.minecraft.text.Text
 
 internal object Constants {
     val ALLOWED_FILE_TYPES_TEXT = "${
-        Component
+        Text
             .translatableWithFallback("trueadaptivemusic.allowed_file_types", "Allowed file types")
             .string
     }: ${ALLOWED_FILE_TYPES.joinToString(", ")}"
-    val MUSIC_CHOICE_TEXT: MutableComponent = Component.translatableWithFallback(
+    val MUSIC_CHOICE_TEXT: MutableText = Text.translatableWithFallback(
         "trueadaptivemusic.music_choice", "Music Choice")
-    val SELECT_TRACKS_TEXT = Component
+    val SELECT_TRACKS_TEXT: MutableText = Text
         .translatableWithFallback("trueadaptivemusic.select_track", "Select tracks")
-    val MUSIC_CHOICE_TOOLTIP_TEXT: MutableComponent = Component.translatableWithFallback(
+    val MUSIC_CHOICE_TOOLTIP_TEXT: MutableText = Text.translatableWithFallback(
         "trueadaptivemusic.music_choice.description",
         "Select any amount of music to be chosen randomly to play"
     ).append("\n\n").append(ALLOWED_FILE_TYPES_TEXT)
