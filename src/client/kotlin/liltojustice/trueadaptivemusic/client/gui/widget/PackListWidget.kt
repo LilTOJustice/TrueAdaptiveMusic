a1526dd0fc3cd67a9f5a7224e60008d21e6ccc6d
LilTOJustice <muianick4@gmail.com>
2026-07-02 23:02:27 -0700

    2.7.0 backport to 1.21.3

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/widget/PackListWidget.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/widget/PackListWidget.kt
@@ -126,7 +126,7 @@ class PackListWidget(
 
             addEntry(
                 PackBrowserEntry {
-                    minecraft.gui.setScreen(
+                    client.setScreen(
                         if (TAMClient.extensions != null)
                             PackBrowserScreen(screen)
                         else
