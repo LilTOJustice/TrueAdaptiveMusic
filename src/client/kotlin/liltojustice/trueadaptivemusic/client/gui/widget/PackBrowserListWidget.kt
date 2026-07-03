429b4d1b46d335be0717b8de3255bbcba90a1a4b
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2026-07-02 21:46:39 -0700

    True Adaptive Music 2.7.0 (#1221)
    * Remove unnecessary version tagging
    
    * Update minecraft version filter
    
    * Bump mod version
    
    * Minimum entity count for EntityNearby and Combat
    
    * Disable resuming node option
    
    * Music weights node option
    
    * Fix serialization issues and add natural numbers to music weights
    
    * Fix guardian not triggering combat for 26.1+
    
    * Mega download support
    
    * Add default value support for arguments
    
    * Fix more argument deserialization
    
    * Fix EntityNearbyPredicate.kt issue
    
    * Separate FFmpeg and pack browser internals into TAM Extensions mod
    
    * Add back curseforge publishing
    
    * Update CHANGELOG.md and README.md
    
    * Switch back to release

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/widget/PackBrowserListWidget.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/widget/PackBrowserListWidget.kt
@@ -6,8 +6,8 @@ import kotlinx.coroutines.runBlocking
 import liltojustice.trueadaptivemusic.Constants
 import liltojustice.trueadaptivemusic.Logger
 import liltojustice.trueadaptivemusic.Reference
+import liltojustice.trueadaptivemusic.client.TAMClient
 import liltojustice.trueadaptivemusic.client.browser.BrowsableMusicPack
-import liltojustice.trueadaptivemusic.client.browser.BrowsableMusicPackDownloader
 import liltojustice.trueadaptivemusic.client.browser.DataSizeHelper
 import liltojustice.trueadaptivemusic.client.browser.DownloadButtonWidget
 import liltojustice.trueadaptivemusic.client.browser.PackManifest
@@ -73,9 +73,8 @@ class PackBrowserListWidget(
         clearEntries()
         backgroundScope.launch {
             try {
-                packManifest = BrowsableMusicPackDownloader.fetchPacksFromRepository(ignoreCache)
+                packManifest = TAMClient.extensions?.packFetcher(ignoreCache)
                 initEntries()
-
                 renderState = RenderState.Success
             }
             catch (e: Exception) {
@@ -317,7 +316,7 @@ class PackBrowserListWidget(
 
             DownloadButtonWidget(packPath in downloadedPacks, oldPack != null, progress) {
                 runBlocking {
-                    BrowsableMusicPackDownloader.downloadMusicPack(musicPack, progress)
+                    TAMClient.extensions?.packDownloader(musicPack, progress) ?: return@runBlocking
                     oldPack?.let { oldPack.deleteIfExists() }
                 }
             }
