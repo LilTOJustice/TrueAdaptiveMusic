7b1ac800262347016e078b26a9ef01565a5cbf3d
LilTOJustice <muianick4@gmail.com>
2026-07-02 22:57:32 -0700

    2.7.0 backport to 1.21.6

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/trigger/predicate/types/EntityNearbyPredicate.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/trigger/predicate/types/EntityNearbyPredicate.kt
@@ -41,7 +41,7 @@ object EntityNearbyPredicate: StaticPredicateType<EntityNearbyPredicate.Argument
 
         return validEntities
             .count {
-                playerEntity.position().distanceTo(it.position()).toUInt() <= arguments.blockRadius
+                playerEntity.pos.distanceTo(it.pos).toUInt() <= arguments.blockRadius
             } >= arguments.minimumCount.toInt()
     }
 }
