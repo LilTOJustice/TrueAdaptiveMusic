37728a4f92dd3ed47714d50551e3330cd23fffef
LilTOJustice <muianick4@gmail.com>
2026-07-02 22:52:52 -0700

    2.7.0 backport to 1.21.8

--- src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/screen/ExtensionsSuggestionScreen.kt
+++ src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/screen/ExtensionsSuggestionScreen.kt
@@ -3,89 +3,89 @@ package liltojustice.trueadaptivemusic.client.gui.screen
 import liltojustice.trueadaptivemusic.Constants
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
-import net.minecraft.client.gui.GuiGraphicsExtractor
-import net.minecraft.client.gui.components.Button
-import net.minecraft.client.gui.components.SpriteIconButton
-import net.minecraft.client.gui.screens.Screen
-import net.minecraft.network.chat.CommonComponents
-import net.minecraft.network.chat.Component
-import net.minecraft.resources.Identifier
-import net.minecraft.util.CommonColors
+import net.minecraft.client.gui.DrawContext
+import net.minecraft.client.gui.screen.Screen
+import net.minecraft.client.gui.widget.ButtonWidget
+import net.minecraft.client.gui.widget.TextIconButtonWidget
+import net.minecraft.screen.ScreenTexts
+import net.minecraft.text.Text
+import net.minecraft.util.Colors
+import net.minecraft.util.Identifier
 import net.minecraft.util.Util
 import kotlin.io.path.*
 
 @Environment(EnvType.CLIENT)
 class ExtensionsSuggestionScreen(private val parent: Screen)
     : Screen(
-    Component.translatableWithFallback(
+    Text.translatableWithFallback(
         "trueadaptivemusic.extensions_suggested", "TAM Extensions Mod Recommended")
     ) {
     @OptIn(ExperimentalPathApi::class)
     override fun init() {
-        val yesButtonWidget = SpriteIconButton.builder(
-            CommonComponents.GUI_YES,
+        val yesButtonWidget = TextIconButtonWidget.builder(
+            ScreenTexts.YES,
             {
-                Util.getPlatform().openUri(Constants.TAM_EXTENSIONS_URL)
-                minecraft.gui.setScreen(parent)
+                Util.getOperatingSystem().open(Constants.TAM_EXTENSIONS_URL)
+                client?.setScreen(parent)
             },
             false
         )
-            .sprite(CHECKMARK, 9, 8)
+            .texture(CHECKMARK, 9, 8)
             .build()
-        val noButtonWidget = Button.Builder(CommonComponents.GUI_NO) {
-            minecraft.gui.setScreen(parent)
+        val noButtonWidget = ButtonWidget.Builder(ScreenTexts.NO) {
+            client?.setScreen(parent)
         }
             .build()
         yesButtonWidget.width = 60
         noButtonWidget.width = 60
         yesButtonWidget.x = width / 2 - 32 - yesButtonWidget.width / 2
         noButtonWidget.x = width / 2 + 32 - noButtonWidget.width / 2
-        yesButtonWidget.y = height / 2 + (font.lineHeight + 5) * 2
+        yesButtonWidget.y = height / 2 + (textRenderer.fontHeight + 5) * 2
         noButtonWidget.y = yesButtonWidget.y
 
-        addRenderableWidget(yesButtonWidget)
-        addRenderableWidget(noButtonWidget)
+        addDrawableChild(yesButtonWidget)
+        addDrawableChild(noButtonWidget)
     }
 
-    override fun onClose() {
-        minecraft.gui.setScreen(parent)
+    override fun close() {
+        client?.setScreen(parent)
     }
 
-    override fun extractRenderState(graphics: GuiGraphicsExtractor, mouseX: Int, mouseY: Int, a: Float) {
-        super.extractRenderState(graphics, mouseX, mouseY, a)
-        graphics.centeredText(
-            font,
-            Component.translatableWithFallback(
+    override fun render(graphics: DrawContext, mouseX: Int, mouseY: Int, a: Float) {
+        super.render(graphics, mouseX, mouseY, a)
+        graphics.drawCenteredTextWithShadow(
+            textRenderer,
+            Text.translatableWithFallback(
                 "trueadaptivemusic.extensions_suggested_1",
                 "The True Adaptive Music Extensions mod is required for this, but wasn't found."
             ),
             width / 2,
-            height / 2 - (font.lineHeight + 5),
-            CommonColors.WHITE
+            height / 2 - (textRenderer.fontHeight + 5),
+            Colors.WHITE
         )
-        graphics.centeredText(
-            font,
-            Component.translatableWithFallback(
+        graphics.drawCenteredTextWithShadow(
+            textRenderer,
+            Text.translatableWithFallback(
                 "trueadaptivemusic.extensions_suggested_2",
                 "This is usually because you got TAM from CurseForge, which doesn't support it."
             ),
             width / 2,
             height / 2,
-            CommonColors.WHITE
+            Colors.WHITE
         )
-        graphics.centeredText(
-            font,
-            Component.translatableWithFallback(
+        graphics.drawCenteredTextWithShadow(
+            textRenderer,
+            Text.translatableWithFallback(
                 "trueadaptivemusic.extensions_suggested_2",
                 "Do you want to open the mod page for TAM Extensions on Modrinth?"
             ),
             width / 2,
-            height / 2 + font.lineHeight + 5,
-            CommonColors.WHITE
+            height / 2 + textRenderer.fontHeight + 5,
+            Colors.WHITE
         )
     }
 
     companion object {
-        private val CHECKMARK: Identifier = Identifier.withDefaultNamespace("icon/checkmark")
+        private val CHECKMARK: Identifier = Identifier.ofVanilla("icon/checkmark")
     }
 }
