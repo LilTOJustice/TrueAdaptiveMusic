3d407dcb879379e18be9462ded5189155377a2f5
Nicholas Muia <57810291+liltojustice@users.noreply.github.com>
2026-07-11 15:53:43 -0700

    True Adaptive Music Forge/Neoforge port (#1280)
    * Migrate fabric to subproject
    
    * Migrate fabric to subproject
    
    * Neoforge skeleton
    
    * Add neoforge prefix
    
    * Fix fabric mod id
    
    * Add accesstransformer.cfg
    
    * First successful entrypoint into common
    
    * Launchable neoforge
    
    * Mixins injecting
    
    * Some fixes needed in 2.7
    
    * Reenable mixins
    
    * Fix identifier deserialization
    
    * Networking and reflection
    
    * Neoforge port complete
    
    * Clean up unused neoforge
    
    * Fabric migration to common
    
    * Migrate neoforge to new multiloader interface api
    
    * Migrate out of common package
    
    * Remove unnecessary project include
    
    * Update publish.yml
    
    * Update publish.yml
    
    * Update build scripts
    
    * Update api dependency
    
    * Fix fabric api version range
    
    * Update dependencies
    
    * Remove parchment
    
    * Update build scripts
    
    * Add fabricmc repo
    
    * Another attempt at fixing cloudbuild

---
+++ common/src/main/kotlin/liltojustice/trueadaptivemusic/network/TrueAdaptiveMusicNetworkingException.kt
@@ -0,0 +1,6 @@
+package liltojustice.trueadaptivemusic.network
+
+import liltojustice.trueadaptivemusicapi.TrueAdaptiveMusicException
+
+class TrueAdaptiveMusicNetworkingException(
+    message: String? = null, inner: Exception? = null): TrueAdaptiveMusicException(message, inner)
