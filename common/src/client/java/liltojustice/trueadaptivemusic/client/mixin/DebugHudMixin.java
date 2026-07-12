c301d24bac22bdc302f102f5d57120ef1c9db290
LilTOJustice <muianick4@gmail.com>
2026-07-12 01:27:18 -0700

    fix backport issues

--- common/src/client/java/liltojustice/trueadaptivemusic/client/mixin/DebugHudMixin.java
+++ common/src/client/java/liltojustice/trueadaptivemusic/client/mixin/DebugHudMixin.java
@@ -1,6 +1,5 @@
 package liltojustice.trueadaptivemusic.client.mixin;
 
-import com.llamalad7.mixinextras.sugar.Local;
 import liltojustice.trueadaptivemusic.client.javasucks.DebugHudMixinHelper;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.gui.Gui;
@@ -9,18 +8,11 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(Gui.class)
 public class DebugHudMixin {
-    @Inject(method = "extractRenderState", at = @At("TAIL"), locals = LocalCapture.CAPTURE_FAILSOFT)
-    public void render(
-            DeltaTracker deltaTracker,
-            boolean shouldRenderLevel,
-            boolean resourcesLoaded,
-            CallbackInfo ci,
-            @Local(name = "graphics") GuiGraphicsExtractor graphics
-    ) {
+    @Inject(method = "extractRenderState", at = @At("TAIL"))
+    public void render(GuiGraphicsExtractor graphics, DeltaTracker deltaTracker, CallbackInfo ci) {
         DebugHudMixinHelper.render(graphics);
     }
 }
