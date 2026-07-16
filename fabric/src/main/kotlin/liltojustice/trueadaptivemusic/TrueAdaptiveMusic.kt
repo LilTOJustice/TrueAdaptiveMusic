4acb21286dc777e872b5175c46b859b7db4b34d1
LilTOJustice <muianick4@gmail.com>
2026-07-15 23:16:11 -0700

    Some fixes

--- fabric/src/main/kotlin/liltojustice/trueadaptivemusic/TrueAdaptiveMusic.kt
+++ fabric/src/main/kotlin/liltojustice/trueadaptivemusic/TrueAdaptiveMusic.kt
@@ -1,10 +1,13 @@
 package liltojustice.trueadaptivemusic
 
 import liltojustice.trueadaptivemusic.network.FabricServerNetworkInterface
+import liltojustice.trueadaptivemusic.network.TAMServerNetworking
 import net.fabricmc.api.ModInitializer
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerTickEvents
 
 class TrueAdaptiveMusic: ModInitializer {
     override fun onInitialize() {
         TAMMainInitializer.onInitialize(FabricServerNetworkInterface)
+        ServerTickEvents.END_SERVER_TICK.register(TAMServerNetworking::processTick)
     }
 }
