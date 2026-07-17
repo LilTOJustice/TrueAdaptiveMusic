b6038350a248d8882c66c0b0bbfafcaa4e2dce5a
LilTOJustice <muianick4@gmail.com>
2026-07-16 19:58:32 -0700

    2.7.0 backport to 1.21.4

---
+++ common/src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/widget/utility/CheckboxWidget.kt
@@ -0,0 +1,68 @@
+package liltojustice.trueadaptivemusic.client.gui.widget.utility
+
+import com.mojang.blaze3d.systems.RenderSystem
+import net.minecraft.client.Minecraft
+import net.minecraft.client.gui.Font
+import net.minecraft.client.gui.GuiGraphics
+import net.minecraft.client.gui.components.Checkbox
+import net.minecraft.client.renderer.RenderType
+import net.minecraft.network.chat.Component
+import net.minecraft.util.CommonColors
+import net.minecraft.resources.ResourceLocation
+import kotlin.math.max
+
+class CheckboxWidget(
+    prompt: String,
+    private val onChange: (checked: Boolean) -> Unit,
+    x: Int = 0,
+    y: Int = 0,
+    checked: Boolean = true
+):
+    Checkbox(
+        x,
+        y,
+        0,
+        Component.literal(prompt),
+        Minecraft.getInstance().font,
+        checked,
+        { _, _ -> }
+    ) {
+    val textRenderer: Font = Minecraft.getInstance().font
+
+    init {
+        width = CHECKBOX_SIZE + PADDING + textRenderer.width(prompt)
+        height = max(textRenderer.lineHeight, CHECKBOX_SIZE)
+        onChange(selected())
+    }
+
+    override fun onPress() {
+        super.onPress()
+        onChange(selected())
+    }
+
+    override fun renderWidget(context: GuiGraphics, mouseX: Int, mouseY: Int, delta: Float) {
+        RenderSystem.setShaderColor(1.0f, 1.0f, 1.0f, alpha)
+        context.blitSprite(
+            RenderType::guiTextured,
+            if (selected()) CHECKED else UNCHECKED,
+            x,
+            y,
+            CHECKBOX_SIZE,
+            CHECKBOX_SIZE,
+            CommonColors.WHITE)
+        context.drawString(
+            textRenderer,
+            message,
+            x + CHECKBOX_SIZE + PADDING,
+            y,
+            CommonColors.WHITE
+        )
+    }
+
+    companion object {
+        private val UNCHECKED = ResourceLocation.withDefaultNamespace("widget/checkbox")
+        private val CHECKED = ResourceLocation.withDefaultNamespace("widget/checkbox_selected")
+        private const val PADDING = 5
+        private const val CHECKBOX_SIZE = 10
+    }
+}
---
+++ common/src/main/kotlin/liltojustice/trueadaptivemusic/network/processor/StructureProcessor.kt
@@ -0,0 +1,58 @@
+package liltojustice.trueadaptivemusic.network.processor
+
+import liltojustice.trueadaptivemusic.Constants
+import liltojustice.trueadaptivemusic.network.model.CurrentStructurePayload
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload
+import net.minecraft.core.registries.Registries
+import net.minecraft.server.MinecraftServer
+import net.minecraft.server.level.ServerPlayer
+import net.minecraft.world.level.levelgen.structure.StructurePiece
+import net.minecraft.core.SectionPos
+import net.minecraft.world.level.levelgen.structure.Structure
+
+class StructureProcessor: Processor() {
+    private val structureCache = mutableMapOf<SectionPos, StructureData>()
+    override fun makePacket(server: MinecraftServer, player: ServerPlayer): CustomPacketPayload {
+        val level = player.serverLevel()
+        val structureManager = level.structureManager()
+        val registryAccess = structureManager.registryAccess()
+        val structureRegistry = registryAccess.lookup(Registries.STRUCTURE).get()
+        val structureSetRegistry = registryAccess.lookup(Registries.STRUCTURE_SET).get()
+        val structurePieceRegistry = registryAccess.lookup(Registries.STRUCTURE_PIECE).get()
+        val nearby = structureManager.getAllStructuresAt(player.blockPosition()).keys
+            .map { structure ->
+                val sectionPos = player.lastSectionPos
+                structureCache.getOrPut(sectionPos) {
+                    val starts = structureManager.startsForStructure(sectionPos, structure)
+                    val bounds = starts.takeIf { it.isNotEmpty() }?.let {
+                        starts.maxOf { it.boundingBox.minY() } to starts.minOf { it.boundingBox.maxY() }
+                    }
+
+                    StructureData(structure, starts.flatMap { it.pieces }.toSet(), bounds)
+                }
+            }
+            .firstOrNull { structureData ->
+                structureData.yBounds?.let { yBounds ->
+                    player.blockPosition().y.let { it >= yBounds.first && it <= yBounds.second }
+                } ?: false
+            }
+
+        val structurePieceId = nearby?.let {
+            it.pieces.firstOrNull { piece -> piece.boundingBox.isInside(player.blockPosition()) }
+                ?.let { piece -> structurePieceRegistry.getKey(piece.type) }
+        } ?: Constants.NULL_IDENTIFIER
+        val structureId = nearby?.let { structureRegistry.getKey(it.structure) } ?: Constants.NULL_IDENTIFIER
+        val structureSetId = nearby?.let {
+            structureSetRegistry.toList().firstOrNull { set ->
+                set.structures.any { structureSelection ->
+                    structureId == structureSelection.structure().unwrapKey().get().location()
+                }
+            }?.let { structureSetRegistry.getKey(it) }
+        } ?: Constants.NULL_IDENTIFIER
+
+        return CurrentStructurePayload(
+            structureId, structureSetId, structurePieceId)
+    }
+
+    data class StructureData(val structure: Structure, val pieces: Set<StructurePiece>, val yBounds: Pair<Int, Int>?)
+}
