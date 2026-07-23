b9f1c9968d6e42026b5c43bce72ac492ae8d6795
LilTOJustice <muianick4@gmail.com>
2026-07-22 21:49:06 -0700

    Pre 2.7 changes

---
+++ extensions/src/main/kotlin/liltojustice/trueadaptivemusicextensions/client/serialization/EnumTypeAdapter.kt
@@ -0,0 +1,18 @@
+package liltojustice.trueadaptivemusicextensions.client.serialization
+
+import com.google.gson.TypeAdapter
+import com.google.gson.stream.JsonReader
+import com.google.gson.stream.JsonWriter
+import kotlin.reflect.KClass
+
+class EnumTypeAdapter<T: Enum<*>>(private val enumClass: KClass<T>): TypeAdapter<Enum<*>>() {
+    override fun write(writer: JsonWriter, value: Enum<*>) {
+        writer.value(value.ordinal)
+    }
+
+    override fun read(reader: JsonReader): Enum<*>? {
+        val ordinal = reader.nextInt()
+
+        return enumClass.java.enumConstants[ordinal]
+    }
+}
