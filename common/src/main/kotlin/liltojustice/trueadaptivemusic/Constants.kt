71af0032bcaf28831227c7f75473827dcf9f6aa7
LilTOJustice <muianick4@gmail.com>
2026-07-23 20:58:18 -0700

    2.7 final backport to 1.21.6

--- common/src/main/kotlin/liltojustice/trueadaptivemusic/Constants.kt
+++ common/src/main/kotlin/liltojustice/trueadaptivemusic/Constants.kt
@@ -45,6 +45,6 @@ class Constants {
         const val PREDICATES_DIRNAME = "predicates"
         const val WIKI_LINK = "https://liltojustice.github.io/TrueAdaptiveMusic/"
         const val DISCORD_JOIN_URL = "https://discord.gg/v64K4hNdXu"
-        const val TAM_EXTENSIONS_URL = "https://modrinth.com/project/true-adaptive-music-extensions"
+        const val TAM_MODRINTH_URL = "https://modrinth.com/project/true-adaptive-music"
     }
 }
