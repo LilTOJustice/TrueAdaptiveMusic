f9b32bf27f990414d3bbdb0728f982410de3c222
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2026-07-23 23:52:45 -0700

    2.7.0 backport to 1.21.9 (#1289)
    * 2.7.0 backport to 1.21.9
    
    * Some fixes
    
    * Update api dependency
    
    * 2.7 final backport to 1.21.9

---
+++ common/src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/widget/PackOptionsViewWidget.kt
@@ -0,0 +1,51 @@
+package liltojustice.trueadaptivemusic.client.gui.widget
+
+import liltojustice.trueadaptivemusic.client.gui.widget.utility.ContainerWidget
+import liltojustice.trueadaptivemusic.client.music.pack.MusicPackOptions
+import liltojustice.trueadaptivemusicapi.TAMAPI
+import liltojustice.trueadaptivemusicapi.widget.WidgetArg
+import net.minecraft.client.input.MouseButtonEvent
+import net.minecraft.client.gui.GuiGraphics
+import net.minecraft.client.gui.narration.NarrationElementOutput
+import kotlin.reflect.full.primaryConstructor
+
+class PackOptionsViewWidget(initialOptions: MusicPackOptions, width: Int, height: Int, x: Int = 0, y: Int = 0)
+    : ContainerWidget(width, height, "", false, false, x = x, y = y) {
+    private val requiredOptionsArgs = MusicPackOptions
+        .getRequiredArgs().map { WidgetArg.of(it) }
+    private var optionsArgs: MutableList<Any?> = initialOptions.getArgs().toMutableList()
+
+    override fun updateWidgetNarration(builder: NarrationElementOutput) {
+    }
+
+    override fun mouseClicked(click: MouseButtonEvent, doubled: Boolean): Boolean {
+        super.mouseClicked(click, doubled)
+        return false
+    }
+
+    override fun renderWidget(context: GuiGraphics, mouseX: Int, mouseY: Int, delta: Float) {
+        super.renderWidget(context, mouseX, mouseY, delta)
+        if (!visible) {
+            return
+        }
+
+        requiredOptionsArgs.forEach { required ->
+            addWidgetFromRender(
+                {
+                    TAMAPI.makeInputWidget(
+                        screen!!,
+                        optionsArgs,
+                        required,
+                        required.name?.let { MusicPackOptions.getArgDisplayName(it) },
+                        required.name?.let { MusicPackOptions.getArgDescription(it) }
+                    ) {}
+                },
+                "${required.name}: ${required.type}")
+        }
+    }
+
+    fun getCurrentOptions(): MusicPackOptions {
+        return MusicPackOptions::class.primaryConstructor?.call(*optionsArgs.toTypedArray())
+            ?: MusicPackOptions()
+    }
+}
---
+++ common/src/main/kotlin/liltojustice/trueadaptivemusic/network/processor/Processor.kt
@@ -0,0 +1,22 @@
+package liltojustice.trueadaptivemusic.network.processor
+
+import net.minecraft.network.protocol.common.custom.CustomPacketPayload
+import net.minecraft.server.MinecraftServer
+import net.minecraft.server.level.ServerPlayer
+
+abstract class Processor {
+    protected val tickRate = 20
+    private var tick = tickRate
+
+    fun process(server: MinecraftServer, player: ServerPlayer): CustomPacketPayload? {
+        if (tick++ >= tickRate) {
+            tick = 0
+
+            return makePacket(server, player)
+        }
+
+        return null
+    }
+
+    protected abstract fun makePacket(server: MinecraftServer, player: ServerPlayer): CustomPacketPayload?
+}
