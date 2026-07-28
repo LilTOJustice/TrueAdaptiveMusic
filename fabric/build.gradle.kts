4e0ecb7981ef9f75079c9da69df3198ef59f4290
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2026-07-27 19:48:56 -0700

    1.21.1: Cherry pick commit (#1356)
    * 2.7.4 - Fix server networking
    
    * Update fabric api
    
    * Fix a buncha errors

--- fabric/build.gradle.kts
+++ fabric/build.gradle.kts
@@ -42,6 +42,7 @@ repositories {
     }
 }
 
+val apiFileId = "8518045"
 dependencies {
     minecraft("com.mojang:minecraft:${project.property("minecraft_version")}")
     mappings(loom.officialMojangMappings())
@@ -50,8 +51,8 @@ dependencies {
     modImplementation("net.fabricmc.fabric-api:fabric-api:${project.property("fabric_api_version")}")
 
     modApi("com.terraformersmc:modmenu:${project.property("modMenu_version")}")
-    modApi("curse.maven:trueadaptivemusicapi-1514598:8442524")
-    include("curse.maven:trueadaptivemusicapi-1514598:8442524")
+    modApi("curse.maven:trueadaptivemusicapi-1514598:$apiFileId")
+    include("curse.maven:trueadaptivemusicapi-1514598:$apiFileId")
 }
 
 tasks.processResources {
