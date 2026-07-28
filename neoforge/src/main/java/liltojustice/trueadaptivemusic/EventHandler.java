2d194988b7dd44466a6710ba977651d6f3b9e5d2
LilTOJustice <muianick4@gmail.com>
2026-07-27 18:32:32 -0700

    Fix a buncha errors

--- neoforge/src/main/java/liltojustice/trueadaptivemusic/EventHandler.java
+++ neoforge/src/main/java/liltojustice/trueadaptivemusic/EventHandler.java
@@ -12,10 +12,6 @@ public class EventHandler implements IModBusEvent {
     @SubscribeEvent
     public static void register(final RegisterPayloadHandlersEvent event) {
         NeoforgeServerNetworkingInterface.INSTANCE.registerPayloadHandlers(event);
-    }
-
-    @SubscribeEvent
-    public static void register(RegisterClientPayloadHandlersEvent event) {
         NeoforgeClientNetworkingInterface.INSTANCE.registerPayloadHandlers(event);
     }
 }
