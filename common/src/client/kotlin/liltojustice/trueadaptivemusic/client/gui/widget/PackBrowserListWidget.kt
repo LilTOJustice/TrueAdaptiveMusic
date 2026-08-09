c7ea5b2cba97cec6f24356c6f19c73ea0165dbfc
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2026-08-08 22:49:22 -0700

    1.21.8: Cherry pick commit (#1385)
    * 2.7.9 - Fix log spam in <=1.21.11
    
    * Forgot to bump version to 2.7.9

--- common/src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/widget/PackBrowserListWidget.kt
+++ common/src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/widget/PackBrowserListWidget.kt
@@ -249,7 +249,8 @@ class PackBrowserListWidget(
                     identifier,
                     DynamicTexture(identifier::toString, image)
                 )
-            } ?: return false
+            }
+
             loadedPackImages.add(identifier)
         }
 
@@ -262,8 +263,8 @@ class PackBrowserListWidget(
         val maxImageHeight = y + height - imageY - 3
         var finalImageWidth = image.width
         var finalImageHeight = image.height
-        val widthDiff = (image.width - maxImageWidth)
-        val heightDiff = (image.height - maxImageHeight)
+        val widthDiff = image.width - maxImageWidth
+        val heightDiff = image.height - maxImageHeight
         var xOffset = 0
         var yOffset = 0
 
