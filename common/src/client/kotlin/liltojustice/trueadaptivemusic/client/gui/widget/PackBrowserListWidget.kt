35eec835d29b4077c09fca42d0bb7a0167feb8cf
LilTOJustice <muianick4@gmail.com>
2026-08-08 23:36:00 -0700

    1.20.1 port

--- common/src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/widget/PackBrowserListWidget.kt
+++ common/src/client/kotlin/liltojustice/trueadaptivemusic/client/gui/widget/PackBrowserListWidget.kt
@@ -59,6 +59,7 @@ class PackBrowserListWidget(
     private val downloadedPacks
         get() = Constants.MUSIC_PACK_DIR.toFile().listFiles().filter { it.extension == "zip" }.map { Path(it.path) }
     private val loadedPackImages = mutableSetOf<ResourceLocation>()
+    private val failedPackImages = mutableSetOf<ResourceLocation>()
 
     init {
         reload(firstLoad)
@@ -215,12 +216,20 @@ class PackBrowserListWidget(
                     Util.sanitizeName(imagePath.name, ResourceLocation::validPathChar)
         )
 
+        if (identifier in failedPackImages) {
+            return false
+        }
+
         if (identifier !in loadedPackImages) {
             ImageProcessor.getNativeImage(imagePath)?.let { image ->
                 minecraft.textureManager.register(
                     identifier,
                     DynamicTexture(identifier::toString, image)
                 )
+            } ?: run {
+                failedPackImages.add(identifier)
+
+                return false
             }
 
             loadedPackImages.add(identifier)
