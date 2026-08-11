8b80d9c18d01385d50e87a3e29ef206fc0187b7a
LilTOJustice <muianick4@gmail.com>
2026-08-10 23:02:12 -0700

    2.8 backport to 1.21.11

--- common/src/client/kotlin/liltojustice/trueadaptivemusic/client/TrueAdaptiveMusicOptions.kt
+++ common/src/client/kotlin/liltojustice/trueadaptivemusic/client/TrueAdaptiveMusicOptions.kt
@@ -3,6 +3,7 @@ package liltojustice.trueadaptivemusic.client
 import com.google.gson.GsonBuilder
 import liltojustice.trueadaptivemusic.Constants
 import liltojustice.trueadaptivemusic.ReflectionHelper
+import liltojustice.trueadaptivemusic.client.util.LUFBoost
 import liltojustice.trueadaptivemusic.text.StringExtensions.prettify
 import liltojustice.trueadaptivemusic.text.translatableWithFallbackOrNull
 import net.minecraft.network.chat.Component
@@ -15,7 +16,8 @@ data class TrueAdaptiveMusicOptions(
     val prettifyIdentifiers: Boolean = true,
     val audioNormalization: Boolean = true,
     val musicLoudnessBoost: LUFBoost = LUFBoost(0U),
-    val ambienceLoudnessBoost: LUFBoost = LUFBoost(0U)
+    val ambienceLoudnessBoost: LUFBoost = LUFBoost(0U),
+    val disableAmbience: Boolean = false
 ) {
     fun save() {
         Constants.OPTIONS_PATH.toFile().writeText(jsonEncode())
@@ -46,7 +48,9 @@ data class TrueAdaptiveMusicOptions(
             TrueAdaptiveMusicOptions::musicLoudnessBoost.name to "Increase the music volume by passing a higher " +
                     "LUFS value to FFmpeg. Requires FFmpeg.",
             TrueAdaptiveMusicOptions::ambienceLoudnessBoost.name to "Increase the ambience volume by passing a " +
-                    "higher LUFS value to FFmpeg. Requires FFmpeg."
+                    "higher LUFS value to FFmpeg. Requires FFmpeg.",
+            TrueAdaptiveMusicOptions::disableAmbience.name to "Disables any ambience played by the current pack. " +
+                    "Useful if using an ambience mod that clashes with your selected pack."
         )
 
         private val json = GsonBuilder()
@@ -69,12 +73,6 @@ data class TrueAdaptiveMusicOptions(
         fun getArgDescription(argName: String): Component? {
             return translatableWithFallbackOrNull(
                 "trueadaptivemusic.options.${argName}.description", descriptions[argName])
-        }
-    }
-
-    class LUFBoost(val value: UInt) {
-        companion object {
-            const val MAX_VALUE = 10U
         }
     }
 }
--- neoforge/src/main/kotlin/liltojustice/trueadaptivemusic/TrueAdaptiveMusic.kt
+++ neoforge/src/main/kotlin/liltojustice/trueadaptivemusic/TrueAdaptiveMusic.kt
@@ -2,6 +2,7 @@ package liltojustice.trueadaptivemusic
 
 import liltojustice.trueadaptivemusic.client.NeoforgeModReflectionInterface
 import liltojustice.trueadaptivemusic.client.TAMClientInitializer
+import liltojustice.trueadaptivemusic.client.gui.ConfigScreenFactory
 import liltojustice.trueadaptivemusic.client.network.NeoforgeClientNetworkingInterface
 import liltojustice.trueadaptivemusic.network.NeoforgeServerNetworkingInterface
 import net.neoforged.bus.api.SubscribeEvent
@@ -10,6 +11,7 @@ import net.neoforged.fml.common.Mod
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent
 import net.neoforged.fml.event.lifecycle.FMLCommonSetupEvent
 import net.neoforged.fml.event.lifecycle.FMLDedicatedServerSetupEvent
+import net.neoforged.neoforge.client.gui.IConfigScreenFactory
 import thedarkcolour.kotlinforforge.neoforge.forge.MOD_BUS
 import thedarkcolour.kotlinforforge.neoforge.forge.runForDist
 
@@ -32,6 +34,8 @@ object TrueAdaptiveMusic {
             NeoforgeClientNetworkingInterface,
             NeoforgeModReflectionInterface
         )
+
+        event.container.registerExtensionPoint(IConfigScreenFactory::class.java, ConfigScreenFactory)
     }
 
     @SubscribeEvent
