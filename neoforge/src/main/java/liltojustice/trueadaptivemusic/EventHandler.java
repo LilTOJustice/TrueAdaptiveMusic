da43d57a349da74a1deaf13aad87ec6ad6192451
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2026-08-11 00:09:02 -0700

    2.8 backport to 1.21.9 (#1414)

--- neoforge/src/main/java/liltojustice/trueadaptivemusic/EventHandler.java
+++ neoforge/src/main/java/liltojustice/trueadaptivemusic/EventHandler.java
@@ -9,7 +9,7 @@ import net.neoforged.neoforge.client.network.event.RegisterClientPayloadHandlers
 import net.neoforged.neoforge.event.tick.ServerTickEvent;
 import net.neoforged.neoforge.network.event.RegisterPayloadHandlersEvent;
 
-@EventBusSubscriber(modid = "trueadaptivemusic")
+@EventBusSubscriber(modid = Constants.TRUE_ADAPTIVE_MUSIC)
 public class EventHandler {
     @SubscribeEvent
     public static void register(RegisterPayloadHandlersEvent event) {
