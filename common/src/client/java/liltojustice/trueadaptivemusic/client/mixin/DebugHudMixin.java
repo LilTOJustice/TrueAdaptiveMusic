e8ba93632c185e43dee90929671d7f07438c5662
Nicholas Muia <57810291+LilTOJustice@users.noreply.github.com>
2026-09-07 17:28:31 -0700

    True Adaptive Music 2.9.0 (#1435)
    * Version bump 2.9
    
    * Fix kff dependency
    
    * Increase text box character limit from 32 -> 1000
    
    * ItemPredicate
    
    * Fix typo in EntityNearbyPredicate.kt
    
    * ItemNearby predicate
    
    * Screen Predicate
    
    * Fix wither combat
    
    * Update api dependency
    
    * Ignore persistent node music node option
    
    * Node titles
    
    * Fix mod description
    
    * Fix empty structure array handling
    
    * Option to hide TAM button
    
    * Fix crash and persistence
    
    * Fix crash on pack name screen
    
    * Update OS handling
    
    * Change null stream handling in resource copy
    
    * Update changelog
    
    * Update changelog

--- common/src/client/java/liltojustice/trueadaptivemusic/client/mixin/DebugHudMixin.java
+++ common/src/client/java/liltojustice/trueadaptivemusic/client/mixin/DebugHudMixin.java
@@ -1,5 +1,7 @@
 package liltojustice.trueadaptivemusic.client.mixin;
 
+import com.llamalad7.mixinextras.sugar.Local;
+import liltojustice.trueadaptivemusic.Logger;
 import liltojustice.trueadaptivemusic.client.javasucks.DebugHudMixinHelper;
 import net.minecraft.client.DeltaTracker;
 import net.minecraft.client.gui.Gui;
@@ -11,10 +13,19 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Gui.class)
 public class DebugHudMixin {
-    @Inject(
-            method = "render(Lnet/minecraft/client/gui/GuiGraphics;Lnet/minecraft/client/DeltaTracker;)V",
-            at = @At("HEAD"))
-    public void render(GuiGraphics context, DeltaTracker tickCounter, CallbackInfo ci) {
-        DebugHudMixinHelper.render(context);
+    @Inject(method = "extractRenderState", at = @At("TAIL"), locals = LocalCapture.CAPTURE_FAILSOFT)
+    public void render(
+            DeltaTracker deltaTracker,
+            boolean shouldRenderLevel,
+            boolean resourcesLoaded,
+            CallbackInfo ci,
+            @Local(name = "graphics") GuiGraphicsExtractor graphics
+    ) {
+        try {
+            DebugHudMixinHelper.render(graphics);
+        }
+        catch (Exception e) {
+            Logger.INSTANCE.logError(e.getMessage(), false);
+        }
     }
 }
